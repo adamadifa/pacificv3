c7e1fcf289672652576afbb76f3878328b695a2a
Adam Adifa <adamabdi.al.a@gmail.com>
2022-01-31 18:16:51 +0700

    Update Login

--- resources/views/Auth/login.blade.php
+++ resources/views/Auth/login.blade.php
@@ -1,97 +1,50 @@
 <!DOCTYPE html>
-<!--
-Template Name: Midone - HTML Admin Dashboard Template
-Author: Left4code
-Website: http://www.left4code.com/
-Contact: muhammadrizki@left4code.com
-Purchase: https://themeforest.net/user/left4code/portfolio
-Renew Support: https://themeforest.net/user/left4code/portfolio
-License: You must have a valid license purchased only from themeforest(the above link) in order to legally use the theme for your project.
--->
+<!-- saved from url=(0063)https://www.bootstrapdash.com/demo/login-templates-pro/login-1/ -->
 <html lang="en">
-<!-- BEGIN: Head -->
 
 <head>
-    <meta charset="utf-8">
-    <link href="{{asset('dist/images/logo.svg')}}" rel="shortcut icon">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description"
-        content="Midone admin is super flexible, powerful, clean & modern responsive tailwind admin template with unlimited possibilities.">
-    <meta name="keywords"
-        content="admin template, Midone admin template, dashboard template, flat admin template, responsive admin template, web app">
-    <meta name="author" content="LEFT4CODE">
-    <title>Login - Kopontren Tswarwah</title>
-    <!-- BEGIN: CSS Assets-->
-    <link rel="stylesheet" href="{{asset('dist/css/app.css')}}" />
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Aref+Ruqaa:wght@700&family=Yesteryear&display=swap"
-        rel="stylesheet">
-    <!-- END: CSS Assets-->
-</head>
-<!-- END: Head -->
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 
-<body class="login">
-    <div class="container sm:px-10">
-        <div class="block xl:grid grid-cols-2 gap-4">
-            <!-- BEGIN: Login Info -->
-            <div class="hidden xl:flex flex-col min-h-screen">
-                <a href="" class="-intro-x flex items-center pt-5">
-                    <img alt="Midone Tailwind HTML Admin Template" class="w-20"
-                        src="{{asset('dist/images/tsarwah.png')}}">
-                </a>
-                <div class="my-auto">
-                    <img alt="Midone Tailwind HTML Admin Template" class="-intro-x w-1/2 -mt-16"
-                        src="{{asset('dist/images/vector.png')}}">
-                    <div class="-intro-x text-2xl text-white" style="font-family: 'Aref Ruqaa', serif;">Kopontren
-                        Tsarwah</div>
-                    <div class="-intro-x text-white font-medium text-4xl leading-tight mt-2"
-                        style="font-family: 'Yesteryear', cursive;">
-                        Manfaat & Maslahat
-                    </div>
-                    <div class="-intro-x text-medium text-white">Jln. Raya Ancol No. 27 Kecamatan Sindangkasih Kabupaten
-                        Ciamis</div>
-                </div>
-            </div>
-            <!-- END: Login Info -->
-            <!-- BEGIN: Login Form -->
-            <div class="h-screen xl:h-auto flex py-5 xl:py-0 my-10 xl:my-0">
-                <div
-                    class="my-auto mx-auto xl:ml-20 bg-white xl:bg-transparent px-5 sm:px-8 py-8 xl:p-0 rounded-md shadow-md xl:shadow-none w-full sm:w-3/4 lg:w-2/4 xl:w-auto">
-                    <h2 class="intro-x font-bold text-2xl xl:text-3xl text-center xl:text-left">
-                        Sign In
-                    </h2>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login Template</title>
+    <link rel="stylesheet" href="{{asset('pacific/css/style.css')}}">
+    <link rel="stylesheet" href="{{asset('pacific/css/materialdesignicons.min.css')}}">
+    <link rel="stylesheet" href="{{asset('pacific/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('pacific/css/login.css')}}">
+</head>
+
+<body class="d-flex align-items-center justify-content-center">
+    <section class="overlay">
+        <div class="container-fluid">
+            <div class="card login-card">
+                <img src="{{asset('pacific/css/login.png')}}" alt="login" class="login-card-img">
+                <div class="card-body">
+                    <img src="{{asset('pacific/css/logo.png')}}" width="250px" height="70px" alt="">
+                    <h2 class="login-card-title">Login</h2>
+                    <p class="login-card-description">Silahkan Login Untuk Melanjutkan</p>
                     <form action="/postlogin" method="POST">
                         @csrf
-
-                        <div class="intro-x mt-8">
-                            <input type="text" name="email"
-                                class="intro-x login__input input input--lg border border-gray-300 block"
-                                placeholder="Email">
-                            <input type="password" name="password"
-                                class="intro-x login__input input input--lg border border-gray-300 block mt-4"
+                        <div class="form-group">
+                            <label for="email" class="sr-only">Email</label>
+                            <input type="email" name="email" id="email" class="form-control" placeholder="Email">
+                        </div>
+                        <div class="form-group">
+                            <label for="password" class="sr-only">Password</label>
+                            <input type="password" name="password" id="password" class="form-control"
                                 placeholder="Password">
                         </div>
-                        <div class="intro-x flex text-gray-700 text-xs sm:text-sm mt-4">
-                            <div class="flex items-center mr-auto">
-                                <input type="checkbox" class="input border mr-2" id="remember-me">
-                                <label class="cursor-pointer select-none" for="remember-me">Remember me</label>
-                            </div>
-                            <a href="">Forgot Password?</a>
-                        </div>
-                        <div class="intro-x mt-5 xl:mt-8 text-center xl:text-left">
-                            <button class="button button--lg w-full xl:w-32 text-white xl:mr-3"
-                                style="background-color: #054c2e">Login</button>
-                        </div>
+                        <button type="submit" class="btn btn-primary btn-lg btn-block">LOGIN</button>
                     </form>
+
                 </div>
             </div>
-            <!-- END: Login Form -->
         </div>
-    </div>
-    <!-- BEGIN: JS Assets-->
-    <script src="{{asset('dist/js/app.js')}}"></script>
-    <!-- END: JS Assets-->
+    </section>
+    <script src="{{asset('pacific/js/jquery-3.4.1.min.js')}}"></script>
+    <script src="{{asset('pacific/js/popper.min.js')}}"></script>
+    <script src="{{asset('pacific/js/bootstrap.min.js')}}"></script>
+
 </body>
 
 </html>
