75b185325d164fd2e8edd315752e7f04296eead3
Adam Adifa <adamabdi.al.a@gmail.com>
2022-05-18 13:59:45 +0700

    Update Faktur

--- resources/views/penjualan/laporan/cetakfaktur.blade.php
+++ resources/views/penjualan/laporan/cetakfaktur.blade.php
@@ -296,7 +296,7 @@
                 <tr>
                     <td colspan="4"></td>
                     <td colspan="3" align="center">Total Pembayaran</td>
-                    <td align="right">{{ rupiah($faktur->total-$faktur->potongan) }}</td>
+                    <td align="right">{{ rupiah($faktur->total + $faktur->totalretur-$faktur->potongan) }}</td>
                 </tr>
             </table>
         </td>
