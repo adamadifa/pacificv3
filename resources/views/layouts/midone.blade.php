3568f7f004eefeffd911f84a74476a26832e850a
Adam Adifa <adamabdi.al.a@gmail.com>
2022-05-20 10:20:36 +0700

    Update Template

--- resources/views/layouts/midone.blade.php
+++ resources/views/layouts/midone.blade.php
@@ -19,7 +19,7 @@
 $level = Auth::user()->level;
 
 @endphp
-<body class="vertical-layout vertical-menu-modern dark-layout 2-columns  navbar-floating footer-static  " data-open="click" data-menu="vertical-menu-modern" data-col="2-columns">
+<body class="vertical-layout vertical-menu-modern 2-columns  navbar-floating footer-static  " data-open="click" data-menu="vertical-menu-modern" data-col="2-columns">
 
     @include('layouts.navheader')
     @include('layouts.navbar')
