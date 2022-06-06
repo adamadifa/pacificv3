a916fdbc134239b576fdc30512a6a7ab84d2d1b9
Adam Adifa <adamabdi.al.a@gmail.com>
2022-06-06 21:05:01 +0700

    Update Cost September 2021

--- app/Http/Controllers/LedgerController.php
+++ app/Http/Controllers/LedgerController.php
@@ -569,7 +569,7 @@ class LedgerController extends Controller
 
     public function updatecostratio()
     {
-        $dari = "2021-08-01";
+        $dari = "2021-09-01";
         $sampai = date("Y-m-t", strtotime($dari));
         $ledger = DB::table('ledger_bank')
             ->whereBetween('tgl_ledger', [$dari, $sampai])
@@ -580,7 +580,7 @@ class LedgerController extends Controller
             ->where('peruntukan', 'PC')
             ->get();
 
-        $kode = "CR0821";
+        $kode = "CR0921";
         $cr = DB::table('costratio_biaya')
             ->select('kode_cr')
             ->whereRaw('LEFT(kode_cr,6) ="' . $kode . '"')
