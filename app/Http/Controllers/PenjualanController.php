4b8d3f179e5d334f2a44ee71512242b804374202
Adam Adifa <adamabdi.al.a@gmail.com>
2022-06-13 09:10:08 +0700

    Update Laporan Format Komisi

--- app/Http/Controllers/PenjualanController.php
+++ app/Http/Controllers/PenjualanController.php
@@ -2540,21 +2540,21 @@ class PenjualanController extends Controller
                 $query->leftJoin(
                     DB::raw("(
                     SELECT dp.no_fak_penj,
-                    SUM(IF(kode_produk = 'AB',jumlah,0)) as AB,
-                    SUM(IF(kode_produk = 'AR',jumlah,0)) as AR,
-                    SUM(IF(kode_produk = 'AS',jumlah,0)) as `AS`,
-                    SUM(IF(kode_produk = 'BB',jumlah,0)) as BB,
-                    SUM(IF(kode_produk = 'CG',jumlah,0)) as CG,
-                    SUM(IF(kode_produk = 'CGG',jumlah,0)) as CGG,
-                    SUM(IF(kode_produk = 'DEP',jumlah,0)) as DEP,
-                    SUM(IF(kode_produk = 'DK',jumlah,0)) as DK,
-                    SUM(IF(kode_produk = 'DS',jumlah,0)) as DS,
-                    SUM(IF(kode_produk = 'SP',jumlah,0)) as SP,
-                    SUM(IF(kode_produk = 'BBP',jumlah,0)) as BBP,
-                    SUM(IF(kode_produk = 'SPP',jumlah,0)) as SPP,
-                    SUM(IF(kode_produk = 'CG5',jumlah,0)) as CG5,
-                    SUM(IF(kode_produk = 'SC',jumlah,0)) as SC,
-                    SUM(IF(kode_produk = 'SP8',jumlah,0)) as SP8
+                    SUM(IF(kode_produk = 'AB' AND promo != 1 OR kode_produk ='AB' AND promo IS NULL,jumlah,0)) as AB,
+                    SUM(IF(kode_produk = 'AR' AND promo != 1 OR kode_produk ='AR' AND promo IS NULL,jumlah,0)) as AR,
+                    SUM(IF(kode_produk = 'AS' AND promo != 1 OR kode_produk ='AS' AND promo IS NULL,jumlah,0)) as `AS`,
+                    SUM(IF(kode_produk = 'BB' AND promo != 1 OR kode_produk ='BB' AND promo IS NULL,jumlah,0)) as BB,
+                    SUM(IF(kode_produk = 'CG' AND promo != 1 OR kode_produk ='CG' AND promo IS NULL,jumlah,0)) as CG,
+                    SUM(IF(kode_produk = 'CGG' AND promo != 1 OR kode_produk ='CGG' AND promo IS NULL,jumlah,0)) as CGG,
+                    SUM(IF(kode_produk = 'DEP' AND promo != 1 OR kode_produk ='DEP' AND promo IS NULL,jumlah,0)) as DEP,
+                    SUM(IF(kode_produk = 'DK' AND promo != 1 OR kode_produk ='DK' AND promo IS NULL,jumlah,0)) as DK,
+                    SUM(IF(kode_produk = 'DS' AND promo != 1 OR kode_produk ='DS' AND promo IS NULL,jumlah,0)) as DS,
+                    SUM(IF(kode_produk = 'SP' AND promo != 1 OR kode_produk ='SP' AND promo IS NULL,jumlah,0)) as SP,
+                    SUM(IF(kode_produk = 'BBP' AND promo != 1 OR kode_produk ='BBP' AND promo IS NULL,jumlah,0)) as BBP,
+                    SUM(IF(kode_produk = 'SPP' AND promo != 1 OR kode_produk ='SPP' AND promo IS NULL,jumlah,0)) as SPP,
+                    SUM(IF(kode_produk = 'CG5' AND promo != 1 OR kode_produk ='CG5' AND promo IS NULL,jumlah,0)) as CG5,
+                    SUM(IF(kode_produk = 'SC' AND promo != 1 OR kode_produk ='SC' AND promo IS NULL,jumlah,0)) as SC,
+                    SUM(IF(kode_produk = 'SP8' AND promo != 1 OR kode_produk ='SP8' AND promo IS NULL,jumlah,0)) as SP8
                     FROM detailpenjualan dp
                     INNER JOIN barang b ON dp.kode_barang = b.kode_barang
                     GROUP BY dp.no_fak_penj
