628385b71f6bca7c92e418c5d368fc50593bd6d1
Adam Adifa <adamabdi.al.a@gmail.com>
2022-06-15 15:29:19 +0700

    Update Footer

--- resources/views/layouts/midone.blade.php
+++ resources/views/layouts/midone.blade.php
@@ -20,66 +20,67 @@ $level = Auth::user()->level;
 
 @endphp
 <body class="vertical-layout vertical-menu-modern 2-columns  navbar-floating footer-static" data-open="click" data-menu="vertical-menu-modern" data-col="2-columns">
-    <div id="layout">
-
-
-        @include('layouts.navheader')
-        @include('layouts.navbar')
 
 
 
-
-        <!-- BEGIN: Content-->
-
-        <div class="app-content content">
-            <div class="content-overlay"></div>
-            <div class="header-navbar-shadow"></div>
-            @yield('content')
-            <!-- BEGIN: Customizer-->
-            <div class="customizer d-none d-md-block"><a class="customizer-toggle d-flex align-items-center justify-content-center" href="#"><i class="spinner-grow white"></i></a>
-                <div class="customizer-content">
-                    <!-- Customizer header -->
-                    <div class="customizer-header px-2 pt-1 pb-0 position-relative">
-                        <h4 class="mb-0">Daftar User Online</h4>
-                        <p class="m-0">User Online</p>
-
-                        <a class="customizer-close" href="#"><i data-feather="x"></i></a>
-                    </div>
-                    <hr>
-                    @foreach ($users as $d)
-                    <div class="customizer-menu px-2">
-
-                        <div id="customizer-menu-collapsible" class="d-flex justify-content-start align-items-center">
-                            <div class="avatar mr-50">
-                                <img src="{{ asset('app-assets/images/avatar.png') }}" alt="avtar img holder" height="35" width="35">
-                            </div>
-                            <div class="user-page-info ml-1">
-                                <p class="mt-1">{{ $d->name }}<br><small>Last Seen {{ Carbon\Carbon::parse($d->last_seen)->diffForHumans() }}</small></p>
-                            </div>
-                            @if(Cache::has('user-is-online-' . $d->id))
-                            <div class="ml-auto"><i class="fa fa-circle success"></i></div>
-                            @else
-                            <div class="ml-auto"><i class="fa fa-circle danger"></i></div>
-                            @endif
+    @include('layouts.navheader')
+    @include('layouts.navbar')
 
 
-                        </div>
 
-                    </div>
-                    @endforeach
+
+    <!-- BEGIN: Content-->
+
+    <div class="app-content content">
+        <div class="content-overlay"></div>
+        <div class="header-navbar-shadow"></div>
+        @yield('content')
+        <!-- BEGIN: Customizer-->
+        <div class="customizer d-none d-md-block"><a class="customizer-toggle d-flex align-items-center justify-content-center" href="#"><i class="spinner-grow white"></i></a>
+            <div class="customizer-content">
+                <!-- Customizer header -->
+                <div class="customizer-header px-2 pt-1 pb-0 position-relative">
+                    <h4 class="mb-0">Daftar User Online</h4>
+                    <p class="m-0">User Online</p>
+
+                    <a class="customizer-close" href="#"><i data-feather="x"></i></a>
                 </div>
+                <hr>
+                @foreach ($users as $d)
+                <div class="customizer-menu px-2">
+
+                    <div id="customizer-menu-collapsible" class="d-flex justify-content-start align-items-center">
+                        <div class="avatar mr-50">
+                            <img src="{{ asset('app-assets/images/avatar.png') }}" alt="avtar img holder" height="35" width="35">
+                        </div>
+                        <div class="user-page-info ml-1">
+                            <p class="mt-1">{{ $d->name }}<br><small>Last Seen {{ Carbon\Carbon::parse($d->last_seen)->diffForHumans() }}</small></p>
+                        </div>
+                        @if(Cache::has('user-is-online-' . $d->id))
+                        <div class="ml-auto"><i class="fa fa-circle success"></i></div>
+                        @else
+                        <div class="ml-auto"><i class="fa fa-circle danger"></i></div>
+                        @endif
+
+
+                    </div>
+
+                </div>
+                @endforeach
             </div>
-            <!-- End: Customizer-->
-
         </div>
-        <!-- END: Content-->
-
-
-
-        @include('layouts.footer')
-
-        @include('layouts.script')
+        <!-- End: Customizer-->
     </div>
+    <!-- END: Content-->
+
+
+    <div class="sidenav-overlay"></div>
+    <div class="drag-target"></div>
+
+
+    @include('layouts.footer')
+
+    @include('layouts.script')
 </body>
 <!-- END: Body-->
 
