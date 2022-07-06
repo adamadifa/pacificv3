c8837803092ea448d64fdc5ad893b004f0244181
Adam Adifa <adamabdi.al.a@gmail.com>
2022-07-06 15:49:21 +0700

    Lap Jurnal Umum

--- resources/views/laporanaccounting/laporan/frm/lap_jurnalumum.blade.php
+++ resources/views/laporanaccounting/laporan/frm/lap_jurnalumum.blade.php
@@ -38,43 +38,43 @@
                                             <x-inputtext label="Sampai" field="sampai" icon="feather icon-calendar" datepicker />
                                         </div>
                                     </div>
-                                    <div class="row">
+                                    {{-- <div class="row">
                                         <div class="col-12">
                                             <div class="form-group">
                                                 <select name="kode_dept" id="kode_dept" class="form-control select2">
                                                     <option value="">Semua Departemen</option>
                                                     @foreach ($departemen as $d)
                                                     <option value="{{ $d->kode_dept }}">{{ $d->nama_dept }}</option>
-                                                    @endforeach
-                                                </select>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col-lg-8 col-sm-12">
-                                            <div class="form-group">
-                                                <button type="submit" name="submit" class="btn btn-primary btn-block"><i class="feather icon-printer"></i> Cetak</button>
-                                            </div>
-                                        </div>
-                                        <div class="col-lg-4 col-sm-12">
-                                            <div class="form-group">
-                                                <button type="submit" name="export" class="btn btn-success btn-block"><i class="feather icon-download"></i> Export</button>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </form>
-                                <!-- DataTable ends -->
+                                    @endforeach
+                                    </select>
+                            </div>
+                        </div>
+                    </div> --}}
+                    <div class="row">
+                        <div class="col-lg-8 col-sm-12">
+                            <div class="form-group">
+                                <button type="submit" name="submit" class="btn btn-primary btn-block"><i class="feather icon-printer"></i> Cetak</button>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-sm-12">
+                            <div class="form-group">
+                                <button type="submit" name="export" class="btn btn-success btn-block"><i class="feather icon-download"></i> Export</button>
                             </div>
                         </div>
                     </div>
+                    </form>
+                    <!-- DataTable ends -->
                 </div>
             </div>
-            <div class="col-lg-3 col-sm-12">
-                @include('layouts.nav_laporanaccounting')
-            </div>
         </div>
-        <!-- Data list view end -->
     </div>
+</div>
+<div class="col-lg-3 col-sm-12">
+    @include('layouts.nav_laporanaccounting')
+</div>
+</div>
+<!-- Data list view end -->
+</div>
 </div>
 @endsection
 @push('myscript')
