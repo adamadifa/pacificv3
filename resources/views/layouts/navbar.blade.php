60c25bc49b255d160385453bdf9bdf5bafb83751
Adam Adifa <adamabdi.al.a@gmail.com>
2022-08-12 13:46:53 +0700

    Update Menu Pa Pur

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -475,6 +475,13 @@
             <li class=" nav-item">
                 <a href="#"><i class="feather icon-radio info"></i><span class="menu-title">Marketing</span></a>
                 <ul class="menu-content">
+                    @if ($level=="kepala admin" && $getcbg == "TSM")
+                    <li class="{{ request()->is(['permintaanpengiriman', 'permintaanpengiriman/*']) ? 'active' : '' }}">
+                        <a href="/permintaanpengiriman">
+                            <i class="feather icon-truck"></i>
+                            <span class="menu-item">Permintaan Kirim</span></a>
+                    </li>
+                    @endif
                     @if (in_array($level, $permintaanpengiriman))
                     <li class="{{ request()->is(['permintaanpengiriman', 'permintaanpengiriman/*']) ? 'active' : '' }}">
                         <a href="/permintaanpengiriman">
