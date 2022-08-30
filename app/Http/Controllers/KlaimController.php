39fcb4baed9f2924f424dd8bf8155e305a021c38
Adam Adifa <adamabdi.al.a@gmail.com>
2022-08-30 11:07:38 +0700

    Update Batal Validasi

--- app/Http/Controllers/KlaimController.php
+++ app/Http/Controllers/KlaimController.php
@@ -436,8 +436,7 @@ class KlaimController extends Controller
     {
         $no_bukti = Crypt::decrypt($no_bukti);
         $kaskecil = DB::table('kaskecil_detail')->where('nobukti', $no_bukti)->first();
-        dd($no_bukti);
-        $nobukti_bukubesar = $kaskecil->nobukti_bukubesar != null ?  $kaskecil->nobukti_bukubesar : '';
+        $nobukti_bukubesar = $kaskecil != null ?  $kaskecil->nobukti_bukubesar : '';
         DB::beginTransaction();
         try {
             DB::table('buku_besar')->where('no_bukti', $nobukti_bukubesar)->delete();
