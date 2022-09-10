170e879ed4fd66766c54a0e555d776cdfae80c90
Adam Adifa <adamabdi.al.a@gmail.com>
2022-09-10 11:03:31 +0700

    Update Akun PWK

--- app/Http/Controllers/TransferController.php
+++ app/Http/Controllers/TransferController.php
@@ -182,6 +182,8 @@ class TransferController extends Controller
             $akun = "1-1489";
         } else if ($cabang == "KLT") {
             $akun = "1-1490";
+        } else if ($cabang == "PWK") {
+            $akun = "1-1492";
         }
 
         DB::beginTransaction();
