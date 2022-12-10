7516950a8d05093007c0d974e4ddec2772360563
Adam Adifa <adamabdi.al.a@gmail.com>
2022-12-10 10:03:49 +0700

    Update

--- app/Http/Controllers/PenjualanController.php
+++ app/Http/Controllers/PenjualanController.php
@@ -5525,6 +5525,8 @@ class PenjualanController extends Controller
             pphk,
             vsp,
             kpbpb,
+            wapu,
+            pph22,
             lainnya,
             IFNULL(saldoawalpiutang,0) - IFNULL(piutanglama,0) + IFNULL(piutangpindahanbulanlalu,0) as saldoawalpiutang,
             IFNULL(saldoawalpiutang,0) -  IFNULL(piutanglamanow,0) -  IFNULL(piutanglamaberjalan,0) + IFNULL(piutangpindahan,0) + IFNULL(piutangberjalan,0) + (IFNULL(totalbruto,0) - IFNULL(totalpotongan,0)-IFNULL(totalretur,0) - IFNULL(totalpotistimewa,0) - IFNULL(totalpenyharga,0)) - IFNULL(totalbayarpiutang,0)  as saldoakhirpiutang");
