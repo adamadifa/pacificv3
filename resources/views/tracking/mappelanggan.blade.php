cd6d8c91e45c2453aa187d27f87b757c5af74a5c
Adam Adifa <adamabdi.al.a@gmail.com>
2022-12-22 23:15:41 +0700

    Update

--- resources/views/tracking/mappelanggan.blade.php
+++ resources/views/tracking/mappelanggan.blade.php
@@ -106,9 +106,7 @@
 
                     var imagepath = "{{ Storage::url('pelanggan/') }}" + data[index].foto;
 
-                    var marker = L.marker([parseFloat(data[index].latitude), parseFloat(data[index].longitude)], {
-                        icon: salesmanicon
-                    }).bindPopup("<b>" + data[index].kode_pelanggan + " - " + data[index].nama_pelanggan + "</b><br><br>" + "<img width='200px' src='" + imagepath + "'/><br><br>" + "Latitude : " + data[index].latitude + " <br>Longitude : " + data[index].longitude + "<br> Alamat :" + data[index].alamat_pelanggan, {
+                    var marker = L.circleMarker([parseFloat(data[index].latitude), parseFloat(data[index].longitude)]).bindPopup("<b>" + data[index].kode_pelanggan + " - " + data[index].nama_pelanggan + "</b><br><br>" + "<img width='200px' src='" + imagepath + "'/><br><br>" + "Latitude : " + data[index].latitude + " <br>Longitude : " + data[index].longitude + "<br> Alamat :" + data[index].alamat_pelanggan, {
                         maxWidth: 200
                     });
                     layerGroup.addLayer(marker);
