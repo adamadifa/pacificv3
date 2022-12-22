3ff8aeb936c73b59f4b36eeff0dc7517464efaf8
Adam Adifa <adamabdi.al.a@gmail.com>
2022-12-22 23:13:07 +0700

    Update

--- resources/views/tracking/mappelanggan.blade.php
+++ resources/views/tracking/mappelanggan.blade.php
@@ -77,10 +77,7 @@
     //     , attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     // }).addTo(map);
 
-    L.tileLayer('http://{s}.google.com/vt?lyrs=m&x={x}&y={y}&z={z}', {
-        maxZoom: 20
-        , subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
-    }).addTo(map);
+    L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png").addTo(map);
 
     L.marker([-7.3665114, 108.2148793]).addTo(map);
 
