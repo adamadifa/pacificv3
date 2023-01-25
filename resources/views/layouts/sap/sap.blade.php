ed05a83d3143be1324657b4bb68daed44b95987e
Adam Adifa <adamabdi.al.a@gmail.com>
2023-01-25 11:17:14 +0700

    update

--- resources/views/layouts/sap/sap.blade.php
+++ resources/views/layouts/sap/sap.blade.php
@@ -7,7 +7,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
     <meta name="generator" content="">
-    <title>FiMobile V2.0 - Mobile HTML template</title>
+    <title>SAP - Sales Automation Perfomance</title>
 
     <!-- manifest meta -->
     <meta name="apple-mobile-web-app-capable" content="yes">
@@ -96,87 +96,7 @@
             </div>
 
             <!-- user emnu navigation -->
-            <div class="row">
-                <div class="col-12">
-                    <ul class="nav nav-pills">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="index.html">
-                                <div class="avatar avatar-40 rounded icon"><i class="bi bi-house-door"></i></div>
-                                <div class="col">Dashboard</div>
-                                <div class="arrow"><i class="bi bi-chevron-right"></i></div>
-                            </a>
-                        </li>
-
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">
-                                <div class="avatar avatar-40 rounded icon"><i class="bi bi-person"></i></div>
-                                <div class="col">Account</div>
-                                <div class="arrow"><i class="bi bi-plus plus"></i> <i class="bi bi-dash minus"></i>
-                                </div>
-                            </a>
-                            <ul class="dropdown-menu">
-                                <li><a class="dropdown-item nav-link" href="profile.html">
-                                        <div class="avatar avatar-40 rounded icon"><i class="bi bi-calendar2"></i></div>
-                                        <div class="col">Profile</div>
-                                        <div class="arrow"><i class="bi bi-chevron-right"></i></div>
-                                    </a></li>
-                                <li><a class="dropdown-item nav-link" href="settings.html">
-                                        <div class="avatar avatar-40 rounded icon"><i class="bi bi-calendar-check"></i>
-                                        </div>
-                                        <div class="col">Settings</div>
-                                        <div class="arrow"><i class="bi bi-chevron-right"></i></div>
-                                    </a></li>
-                            </ul>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="chat.html" tabindex="-1">
-                                <div class="avatar avatar-40 rounded icon"><i class="bi bi-chat-text"></i></div>
-                                <div class="col">Messages</div>
-                                <div class="arrow"><i class="bi bi-chevron-right"></i></div>
-                            </a>
-                        </li>
-
-                        <li class="nav-item">
-                            <a class="nav-link" href="notifications.html" tabindex="-1">
-                                <div class="avatar avatar-40 rounded icon"><i class="bi bi-bell"></i></div>
-                                <div class="col">Notification</div>
-                                <div class="arrow"><i class="bi bi-chevron-right"></i></div>
-                            </a>
-                        </li>
-
-                        <li class="nav-item">
-                            <a class="nav-link" href="blog.html" tabindex="-1">
-                                <div class="avatar avatar-40 rounded icon"><i class="bi bi-newspaper"></i></div>
-                                <div class="col">Blogs</div>
-                                <div class="arrow"><i class="bi bi-chevron-right"></i></div>
-                            </a>
-                        </li>
-
-                        <li class="nav-item">
-                            <a class="nav-link" href="style.html" tabindex="-1">
-                                <div class="avatar avatar-40 rounded icon"><i class="bi bi-palette"></i></div>
-                                <div class="col">Style <i class="bi bi-star-fill text-warning small"></i></div>
-                                <div class="arrow"><i class="bi bi-chevron-right"></i></div>
-                            </a>
-                        </li>
-
-                        <li class="nav-item">
-                            <a class="nav-link" href="pages.html" tabindex="-1">
-                                <div class="avatar avatar-40 rounded icon"><i class="bi bi-file-earmark-text"></i></div>
-                                <div class="col">Pages <span class="badge bg-info fw-light">new</span></div>
-                                <div class="arrow"><i class="bi bi-chevron-right"></i></div>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="signin.html" tabindex="-1">
-                                <div class="avatar avatar-40 rounded icon"><i class="bi bi-box-arrow-right"></i></div>
-                                <div class="col">Logout</div>
-                                <div class="arrow"><i class="bi bi-chevron-right"></i></div>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
+            @include('layouts.sap.sidebar')
         </div>
     </div>
     <!-- Sidebar main menu ends -->
@@ -195,7 +115,7 @@
                 <div class="col align-self-center text-center">
                     <div class="logo-small">
                         <img src="assets/img/logo.png" alt="">
-                        <h5>FiMobile</h5>
+                        <h5>SFA</h5>
                     </div>
                 </div>
                 <div class="col-auto">
@@ -210,7 +130,7 @@
 
         <!-- main page content -->
         <div class="main-container container">
-
+            @yield('content')
         </div>
         <!-- main page content ends -->
 
