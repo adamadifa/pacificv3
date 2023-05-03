cf19218c2626cf4001db83f5d5c934068c0a4aec
Adam Adifa <adamabdi.al.a@gmail.com>
2023-05-04 01:37:34 +0700

    update

--- resources/views/layouts/nav_penjualan/navright.blade.php
+++ resources/views/layouts/nav_penjualan/navright.blade.php
@@ -1,3 +1,4 @@
+@if ($level != "salesman")
 <div class="card">
     <div class="card-content">
         <div class="card-body">
@@ -132,6 +133,101 @@
         </ul>
     </div>
 </div>
+@else
+<div class="card">
+    <div class="card-content">
+        <div class="card-body">
+            <h4 class="card-title">Laporan Penjualan</h4>
+        </div>
+        <ul class="list-group list-group-flush">
+            <a href="/laporanpenjualan/penjualan" style="color:#626262">
+                <li class="list-group-item {{ request()->is(['laporanpenjualan/penjualan']) ? 'active' : '' }}">
+                    <i class="feather icon-file mr-1"></i>Penjualan
+                </li>
+            </a>
+
+
+            <a href="/laporankasbesarpenjualan" style="color:#626262">
+                <li class="list-group-item {{ request()->is(['laporankasbesarpenjualan']) ? 'active' : '' }}">
+                    <i class="feather icon-file mr-1"></i>Kas Besar
+                </li>
+            </a>
+
+
+
+
+            <a href="/laporanretur" style="color:#626262">
+                <li class="list-group-item {{ request()->is(['laporanretur']) ? 'active' : '' }}">
+                    <i class="feather icon-file mr-1"></i>Retur
+                </li>
+            </a>
+
+            <a href="/laporanpenjualan/tunaikredit" style="color:#626262">
+                <li class="list-group-item {{ request()->is(['laporanpenjualan/tunaikredit']) ? 'active' : '' }}">
+                    <i class="feather icon-file mr-1"></i>Tunai Kredit
+                </li>
+            </a>
+            <a href="/laporanpenjualan/kartupiutang" style="color:#626262">
+                <li class="list-group-item {{ request()->is(['laporanpenjualan/kartupiutang']) ? 'active' : '' }}">
+                    <i class="feather icon-file mr-1"></i>Kartu Piutang
+                </li>
+            </a>
+            <a href="/laporanpenjualan/aup" style="color:#626262">
+                <li class="list-group-item {{ request()->is(['laporanpenjualan/aup']) ? 'active' : '' }}">
+                    <i class="feather icon-file mr-1"></i>Analisa Umur Piutang
+                </li>
+            </a>
+
+            <a href="/laporanpenjualan/lebihsatufaktur" style="color:#626262">
+                <li class="list-group-item {{ request()->is(['laporanpenjualan/lebihsatufaktur']) ? 'active' : '' }}">
+                    <i class="feather icon-file mr-1"></i>Lebih 1 Faktur
+                </li>
+            </a>
+
+            <a href="/laporanpenjualan/rekapomsetpelanggan" style="color:#626262">
+                <li class="list-group-item {{ request()->is(['laporanpenjualan/rekapomsetpelanggan']) ? 'active' : '' }}">
+                    <i class="feather icon-file mr-1"></i>Rekap Omset Pelanggan
+                </li>
+            </a>
+            <a href="/laporanpenjualan/rekappelanggan" style="color:#626262">
+                <li class="list-group-item {{ request()->is(['laporanpenjualan/rekappelanggan']) ? 'active' : '' }}">
+                    <i class="feather icon-file mr-1"></i>Rekap Pelanggan
+                </li>
+            </a>
+
+            <a href="/laporanpenjualan/rekapwilayah" style="color:#626262">
+                <li class="list-group-item {{ request()->is(['laporanpenjualan/rekapwilayah']) ? 'active' : '' }}">
+                    <i class="feather icon-file mr-1"></i>Rekap Wilayah
+                </li>
+            </a>
+            <a href="/laporanpenjualan/effectivecall" style="color:#626262">
+                <li class="list-group-item {{ request()->is(['laporanpenjualan/effectivecall']) ? 'active' : '' }}">
+                    <i class="feather icon-file mr-1"></i>Effective Call
+                </li>
+            </a>
+
+            <a href="/laporanpenjualan/lhp" style="color:#626262">
+                <li class="list-group-item {{ request()->is(['laporanpenjualan/lhp']) ? 'active' : '' }}">
+                    <i class="feather icon-file mr-1"></i>LHP
+                </li>
+            </a>
+            <a href="/laporanpenjualan/routingsalesman" style="color:#626262">
+                <li class="list-group-item {{ request()->is(['laporanpenjualan/routingsalesman']) ? 'active' : '' }}">
+                    <i class="feather icon-file mr-1"></i>Routing Salesman
+                </li>
+            </a>
+            <a href="/laporanpenjualan/salesperfomance" style="color:#626262">
+                <li class="list-group-item {{ request()->is(['laporanpenjualan/salesperfomance']) ? 'active' : '' }}">
+                    <i class="feather icon-file mr-1"></i>Sales Perfomance
+                </li>
+            </a>
+        </ul>
+    </div>
+</div>
+
+
+@endif
+
 @if (in_array($level, $laporan_komisi))
 <div class="card">
     <div class="card-content">
