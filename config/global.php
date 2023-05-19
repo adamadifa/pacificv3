90367c67cc9fa4af8357cb77e39ea01becbe5fef
Adam Adifa <adamabdi.al.a@gmail.com>
2023-05-19 11:38:17 +0700

    updatE

--- config/global.php
+++ config/global.php
@@ -2,5 +2,5 @@
 return [
     'show_for_hrd' => ['14', '4', '5', '2', '9', '11', '20', '9', '11'],
     'show_for_hrd_2' => "14, 4, 5, 2, 9, 11, 20, 9, 11",
-    'show_all' => ['manager_accounting', 'direktur', 'admin']
+    'show_all' => ['manager accounting', 'direktur', 'admin']
 ];
