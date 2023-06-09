c1f2c0f168148467a10ee0b3a9ed6c7808257af1
Adam Adifa <adamabdi.al.a@gmail.com>
2023-06-10 05:25:03 +0700

    update / g;

--- resources/views/kontrak/cetak.blade.php
+++ resources/views/kontrak/cetak.blade.php
@@ -429,7 +429,7 @@
                 <td style="text-align:center">PIHAK PERTAMA</td>
             </tr>
             <tr>
-                <td style="height: 100px"></td>
+                <td style="height: 90px"></td>
                 <td></td>
                 <td></td>
             </tr>
