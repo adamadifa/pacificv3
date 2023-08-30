a27eb5746772860d4f4d018e9d891d83b0c642c9
Adam Adifa <adamabdi.al.a@gmail.com>
2023-08-30 11:08:46 +0700

    update

--- app/Http/Controllers/VisitController.php
+++ app/Http/Controllers/VisitController.php
@@ -30,7 +30,7 @@ class VisitController extends Controller
             $cabang = DB::table('cabang')->where('kode_cabang', $this->cabang)->get();
         }
         $bln = array("", "Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember");
-        return view('visit.index', compact('bln','cabang','kode_cabang'));
+        return view('visit.index', compact('bln', 'cabang', 'kode_cabang'));
     }
 
     public function show(Request $request)
@@ -39,27 +39,27 @@ class VisitController extends Controller
         $tahun = $request->tahun;
         $cabang = $request->kode_cabang;
 
-        if($cabang == ''){
+        if ($cabang == '') {
             $visit = DB::table('visit')
-            ->join('penjualan','penjualan.no_fak_penj','visit.no_fak_penj')
-            ->join('pelanggan','penjualan.kode_pelanggan','pelanggan.kode_pelanggan')
-            ->join('karyawan','karyawan.id_karyawan','pelanggan.id_sales')
-            ->whereRaw('MONTH(tgl_visit)=' . $bulan)
-            ->whereRaw('YEAR(tgl_visit)=' . $tahun)
-            ->orderBy('visit.kode_cabang','ASC')
-            ->orderBy('visit.tgL_visit','ASC')
-            ->get();
-        }else{
+                ->join('penjualan', 'penjualan.no_fak_penj', 'visit.no_fak_penj')
+                ->join('pelanggan', 'penjualan.kode_pelanggan', 'pelanggan.kode_pelanggan')
+                ->join('karyawan', 'karyawan.id_karyawan', 'pelanggan.id_sales')
+                ->whereRaw('MONTH(tgl_visit)=' . $bulan)
+                ->whereRaw('YEAR(tgl_visit)=' . $tahun)
+                ->orderBy('visit.kode_cabang', 'ASC')
+                ->orderBy('visit.tgL_visit', 'ASC')
+                ->get();
+        } else {
             $visit = DB::table('visit')
-            ->join('penjualan','penjualan.no_fak_penj','visit.no_fak_penj')
-            ->join('pelanggan','penjualan.kode_pelanggan','pelanggan.kode_pelanggan')
-            ->join('karyawan','karyawan.id_karyawan','pelanggan.id_sales')
-            ->whereRaw('MONTH(tgl_visit)=' . $bulan)
-            ->whereRaw('YEAR(tgl_visit)=' . $tahun)
-            ->where('visit.kode_cabang', $request->kode_cabang)
-            ->orderBy('visit.kode_cabang','ASC')
-            ->orderBy('visit.tgL_visit','ASC')
-            ->get();
+                ->join('penjualan', 'penjualan.no_fak_penj', 'visit.no_fak_penj')
+                ->join('pelanggan', 'penjualan.kode_pelanggan', 'pelanggan.kode_pelanggan')
+                ->join('karyawan', 'karyawan.id_karyawan', 'pelanggan.id_sales')
+                ->whereRaw('MONTH(tgl_visit)=' . $bulan)
+                ->whereRaw('YEAR(tgl_visit)=' . $tahun)
+                ->where('visit.kode_cabang', $request->kode_cabang)
+                ->orderBy('visit.kode_cabang', 'ASC')
+                ->orderBy('visit.tgL_visit', 'ASC')
+                ->get();
         }
         return view('visit.show', compact('visit'));
     }
@@ -74,16 +74,16 @@ class VisitController extends Controller
     {
 
         DB::table('visit')
-        ->insert([
-            'kode_cabang' => $request->kode_cabang,
-            'tgl_visit' => $request->tgl_visit,
-            'hasil_konfirmasi' => $request->hasil_konfirmasi,
-            'no_fak_penj' => $request->no_fak_penj,
-            'catatan' => $request->catatan,
-            'action' => $request->action,
-            'nominal' => $request->nominal,
-            'saran' => $request->saran,
-        ]);
+            ->insert([
+                'kode_cabang' => Auth::user()->kode_cabang,
+                'tgl_visit' => $request->tgl_visit,
+                'hasil_konfirmasi' => $request->hasil_konfirmasi,
+                'no_fak_penj' => $request->no_fak_penj,
+                'catatan' => $request->catatan,
+                'action' => $request->action,
+                'nominal' => $request->nominal,
+                'saran' => $request->saran,
+            ]);
         return Redirect::back()->with(['success' => 'Data Berhasil Disimpan']);
     }
 
@@ -105,8 +105,8 @@ class VisitController extends Controller
             $cabang = DB::table('cabang')->where('kode_cabang', $this->cabang)->get();
         }
         $visit = DB::table('visit')
-        ->where('visit.id', $request->id)->first();
-        return view('visit.edit', compact('visit','cabang'));
+            ->where('visit.id', $request->id)->first();
+        return view('visit.edit', compact('visit', 'cabang'));
     }
 
     public function update(Request $request)
@@ -128,7 +128,6 @@ class VisitController extends Controller
         } else {
             return Redirect::back()->with(['warning' => 'Data Gagak Disimpan']);
         }
-
     }
 
     public function laporanVisit()
@@ -139,7 +138,7 @@ class VisitController extends Controller
             $cabang = DB::table('cabang')->where('kode_cabang', $this->cabang)->get();
         }
         $bulan = array("", "Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember");
-        return view('visit.laporan.frm_laporanVisit', compact('cabang','bulan'));
+        return view('visit.laporan.frm_laporanVisit', compact('cabang', 'bulan'));
     }
 
     public function cetakVisit(Request $request)
@@ -147,30 +146,30 @@ class VisitController extends Controller
         $cabang = $request->kode_cabang;
         $bulan = $request->bulan;
         $tahun = $request->tahun;
-        if($cabang == ''){
+        if ($cabang == '') {
             $visit = DB::table('visit')
-            ->select('tgl_visit','visit.kode_cabang','visit.no_fak_penj','action','saran','hasil_konfirmasi','nama_pelanggan','nama_karyawan','pasar','tgltransaksi','jenistransaksi','nominal','catatan')
-            ->join('penjualan','penjualan.no_fak_penj','visit.no_fak_penj')
-            ->join('pelanggan','penjualan.kode_pelanggan','pelanggan.kode_pelanggan')
-            ->join('karyawan','karyawan.id_karyawan','pelanggan.id_sales')
-            ->whereRaw('MONTH(tgl_visit)=' . $bulan)
-            ->whereRaw('YEAR(tgl_visit)=' . $tahun)
-            ->get();
-        }else{
+                ->select('tgl_visit', 'visit.kode_cabang', 'visit.no_fak_penj', 'action', 'saran', 'hasil_konfirmasi', 'nama_pelanggan', 'nama_karyawan', 'pasar', 'tgltransaksi', 'jenistransaksi', 'nominal', 'catatan')
+                ->join('penjualan', 'penjualan.no_fak_penj', 'visit.no_fak_penj')
+                ->join('pelanggan', 'penjualan.kode_pelanggan', 'pelanggan.kode_pelanggan')
+                ->join('karyawan', 'karyawan.id_karyawan', 'pelanggan.id_sales')
+                ->whereRaw('MONTH(tgl_visit)=' . $bulan)
+                ->whereRaw('YEAR(tgl_visit)=' . $tahun)
+                ->get();
+        } else {
             $visit = DB::table('visit')
-            ->select('tgl_visit','visit.kode_cabang','visit.no_fak_penj','action','saran','hasil_konfirmasi','nama_pelanggan','nama_karyawan','pasar','tgltransaksi','jenistransaksi','nominal','catatan')
-            ->join('penjualan','penjualan.no_fak_penj','visit.no_fak_penj')
-            ->join('pelanggan','penjualan.kode_pelanggan','pelanggan.kode_pelanggan')
-            ->join('karyawan','karyawan.id_karyawan','pelanggan.id_sales')
-            ->whereRaw('MONTH(tgl_visit)=' . $bulan)
-            ->whereRaw('YEAR(tgl_visit)=' . $tahun)
-            ->where('visit.kode_cabang', $request->kode_cabang)
-            ->get();
+                ->select('tgl_visit', 'visit.kode_cabang', 'visit.no_fak_penj', 'action', 'saran', 'hasil_konfirmasi', 'nama_pelanggan', 'nama_karyawan', 'pasar', 'tgltransaksi', 'jenistransaksi', 'nominal', 'catatan')
+                ->join('penjualan', 'penjualan.no_fak_penj', 'visit.no_fak_penj')
+                ->join('pelanggan', 'penjualan.kode_pelanggan', 'pelanggan.kode_pelanggan')
+                ->join('karyawan', 'karyawan.id_karyawan', 'pelanggan.id_sales')
+                ->whereRaw('MONTH(tgl_visit)=' . $bulan)
+                ->whereRaw('YEAR(tgl_visit)=' . $tahun)
+                ->where('visit.kode_cabang', $request->kode_cabang)
+                ->get();
         }
         if (isset($_POST['export'])) {
             header("Content-type: application/vnd-ms-excel");
             header("Content-Disposition: attachment; filename=Laporan Visit Pelanggan.xls");
         }
-        return view('visit.laporan.cetak_visit', compact('visit','bulan','tahun','cabang'));
+        return view('visit.laporan.cetak_visit', compact('visit', 'bulan', 'tahun', 'cabang'));
     }
 }
