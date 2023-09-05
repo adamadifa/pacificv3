84170fa71e894906fc64394f9c8d2b2649c4e3fd
Adam Adifa <adamabdi.al.a@gmail.com>
2023-09-05 21:33:16 +0700

    update

--- resources/views/gaji/laporan/cetak_gaji.blade.php
+++ resources/views/gaji/laporan/cetak_gaji.blade.php
@@ -938,9 +938,9 @@
                     <td align="center">{{ !empty($totalhari_shift_3) ? $totalhari_shift_3 : "" }}</td>
                     <td align="right">{{ !empty($totalpremi_shift_3) ? rupiah($totalpremi_shift_3) : "" }}</td>
                     <td align="right">
-                        {{ $jmlinsentif }}
+
                         @php
-                        $bruto = ($upah_perjam * $totaljamkerja) + $jmlinsentif + $total_upah_overtime + $totalpremi_shift_2 + $totalpremi_shift_3;
+                        $bruto = ($upah_perjam * $totaljamkerja) + $total_upah_overtime + $totalpremi_shift_2 + $totalpremi_shift_3;
                         @endphp
                         {{ !empty($bruto) ?  rupiah($bruto) : "" }}
                     </td>
