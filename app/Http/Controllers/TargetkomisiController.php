26d365f658088d0b0887893ab499847393dfb665
Adam Adifa <adamabdi.al.a@gmail.com>
2023-10-23 09:40:40 +0700

    update

--- app/Http/Controllers/TargetkomisiController.php
+++ app/Http/Controllers/TargetkomisiController.php
@@ -3157,7 +3157,7 @@ class TargetkomisiController extends Controller
             // Mendefinisikan nama file ekspor "hasil-export.xls"
             header("Content-Disposition: attachment; filename=Insentif $dari-$sampai.xls");
         }
-        if ($bulan >= 10 && $tahun >= 2022) {
+        if ($bulan >= 10 && $tahun == 2022) {
             echo "A";
             return view('targetkomisi.laporan.cetak_insentif_oktober2022', compact('insentif', 'cabang', 'namabulan', 'bulan', 'tahun'));
         } elseif ($bulan == 9 && $tahun == 2022) {
