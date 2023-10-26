e27cd7160fb24f65f287bb478cc85eac3a503b53
Adam Adifa <adamabdi.al.a@gmail.com>
2023-10-26 14:18:02 +0700

    update

--- app/Helpers/Automaticnumber.php
+++ app/Helpers/Automaticnumber.php
@@ -515,7 +515,7 @@ function hitungjarak($lat1, $lon1, $lat2, $lon2)
 
 function lockreport($tanggal)
 {
-    if ($tanggal < '2021-01-01' && !empty($tanggal)) {
+    if ($tanggal < '2019-01-01' && !empty($tanggal)) {
         echo "Data Belum Ada / Tidak Ditemukan";
         die;
     } else {
@@ -526,14 +526,14 @@ function lockreport($tanggal)
 
 function startreport()
 {
-    $startreport = "2021-01-01";
+    $startreport = "2019-01-01";
     return $startreport;
 }
 
 
 function startyear()
 {
-    $startyear = 2021;
+    $startyear = 2019;
     return $startyear;
 }
 
