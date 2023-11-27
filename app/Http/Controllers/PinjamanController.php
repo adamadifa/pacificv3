f71a7e487a4c645a81ebbf59a5be89999e3968bb
Adam Adifa <adamabdi.al.a@gmail.com>
2023-11-28 02:19:08 +0700

    update

--- app/Http/Controllers/PinjamanController.php
+++ app/Http/Controllers/PinjamanController.php
@@ -126,7 +126,7 @@ class PinjamanController extends Controller
         if ($level == "rsm") {
             $wilayah = Auth::user()->wilayah;
             $wilayah_user = unserialize($wilayah);
-            $query->whereIn('master_karyawan.id_kantor', $wilayah_user);
+            $query->whereIn('master_karyawan.id_kantord', $wilayah_user);
             $query->where('master_karyawan.kode_dept', 'MKT');
             $query->where('nama_jabatan', 'KEPALA PENJUALAN');
             $query->where('id_perusahaan', 'PCF');
