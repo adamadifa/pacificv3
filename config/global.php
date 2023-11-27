be20ad105ccec83b954f83a96dfa114964c5c8c1
Adam Adifa <adamabdi.al.a@gmail.com>
2023-11-28 02:26:14 +0700

    update

--- config/global.php
+++ config/global.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'show_for_hrd' => ['14', '4', '5', '2', '9', '11', '20', '9', '11'],
+    'show_for_hrd' => [14, 4, 5, 2, 9, 11, 20, 9, 11],
     'show_for_hrd_2' => "14, 4, 5, 2, 9, 11, 20, 9, 11",
     'show_all' => ['manager accounting', 'direktur', 'admin', 'emf'],
     'karyawanpage' => ['admin', 'manager hrd', 'kepala admin', 'kepala penjualan', 'manager pembelian', 'kepala gudang', 'manager produksi', 'spv produksi', 'spv maintenance', 'manager accounting', 'manager ga', 'emf', 'manager marketing', 'rsm', 'direktur', 'spv pdqc', 'admin pdqc', 'manager audit'],
