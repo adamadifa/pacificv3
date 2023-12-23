f182a4ac2207383f2b8ec39fed8c618f10424b9e
Adam Adifa <adamabdi.al.a@gmail.com>
2023-12-23 11:16:27 +0700

    update

--- resources/views/presensi/laporan/cetak_psmexcel.blade.php
+++ resources/views/presensi/laporan/cetak_psmexcel.blade.php
@@ -755,13 +755,13 @@
                                     <!-- Menghitung Lembur Hari Libur -->
                                     @if (!empty($ceklemburharilibur))
                                         @php
-                                            $tgl_lembur_dari = $ceklembur[0]['tanggal_dari'];
-                                            $tgl_lembur_sampai = $ceklembur[0]['tanggal_sampai'];
+                                            $tgl_lembur_dari = $ceklemburharilibur[0]['tanggal_dari'];
+                                            $tgl_lembur_sampai = $ceklemburharilibur[0]['tanggal_sampai'];
                                             $jamlembur_dari = date('H:i', strtotime($tgl_lembur_dari));
                                             $jmljam_lbr = hitungjamdesimal($tgl_lembur_dari, $tgl_lembur_sampai);
-                                            $istirahatlbr = $ceklembur[0]['istirahat'] == 1 ? 1 : 0;
+                                            $istirahatlbr = $ceklemburharilibur[0]['istirahat'] == 1 ? 1 : 0;
                                             $jmljam_lembur = $jmljam_lbr > 7 ? 7 : $jmljam_lbr - $istirahatlbr;
-                                            $kategori_lembur = $ceklembur[0]['kategori'];
+                                            $kategori_lembur = $ceklemburharilibur[0]['kategori'];
                                         @endphp
                                         @if (empty($ceklibur) && empty($cekliburpenggantiminggu) && empty($cekwfhfull) && $namahari != 'Minggu')
                                             @if ($jamlembur_dari >= '22:00' && $jmljam_lbr >= 5)
@@ -1042,13 +1042,13 @@
                                 <!-- Menghitung Lembur Hari Libur -->
                                 @if (!empty($ceklemburharilibur))
                                     @php
-                                        $tgl_lembur_dari = $ceklembur[0]['tanggal_dari'];
-                                        $tgl_lembur_sampai = $ceklembur[0]['tanggal_sampai'];
+                                        $tgl_lembur_dari = $ceklemburharilibur[0]['tanggal_dari'];
+                                        $tgl_lembur_sampai = $ceklemburharilibur[0]['tanggal_sampai'];
                                         $jamlembur_dari = date('H:i', strtotime($tgl_lembur_dari));
                                         $jmljam_lbr = hitungjamdesimal($tgl_lembur_dari, $tgl_lembur_sampai);
-                                        $istirahatlbr = $ceklembur[0]['istirahat'] == 1 ? 1 : 0;
+                                        $istirahatlbr = $ceklemburharilibur[0]['istirahat'] == 1 ? 1 : 0;
                                         $jmljam_lembur = $jmljam_lbr > 7 ? 7 : $jmljam_lbr - $istirahatlbr;
-                                        $kategori_lembur = $ceklembur[0]['kategori'];
+                                        $kategori_lembur = $ceklemburharilibur[0]['kategori'];
                                     @endphp
                                     @if (empty($ceklibur) && empty($cekliburpenggantiminggu) && empty($cekwfhfull) && $namahari != 'Minggu')
                                         @if ($jamlembur_dari >= '22:00' && $jmljam_lbr >= 5)
