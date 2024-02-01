679b4419cf75f5c3b1b274dcf7803d3f39d58ce6
adam adifam <adamabdi.al.a@gmail.com>
2024-02-01 15:56:45 +0700

    update

--- resources/views/presensi/laporan/cetak.blade.php
+++ resources/views/presensi/laporan/cetak.blade.php
@@ -170,28 +170,28 @@
                             //Parameter Pencarian Data Libur
                             $search_items = [
                                 'nik' => $d->nik,
-                                'id_kantor' => $d->id_kantor,
+
                                 'tanggal_libur' => $tgl_presensi,
                             ];
 
                             //Parameter Pencarian Data Lembur
                             $search_items_lembur = [
                                 'nik' => $d->nik,
-                                'id_kantor' => $d->id_kantor,
+
                                 'tanggal_lembur' => $tgl_presensi,
                             ];
 
                             //Parameter Penggantian Libur Minggu
                             $search_items_minggumasuk = [
                                 'nik' => $d->nik,
-                                'id_kantor' => $d->id_kantor,
+
                                 'tanggal_diganti' => $tgl_presensi,
                             ];
 
                             //Parameter Pencarian Data Libur
                             $search_items_all = [
                                 'nik' => 'ALL',
-                                'id_kantor' => $d->id_kantor,
+
                                 'tanggal_libur' => $tgl_presensi,
                             ];
 
