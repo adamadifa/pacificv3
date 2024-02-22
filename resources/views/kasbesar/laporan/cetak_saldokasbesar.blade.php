ae8660ba4242c29943acd40c726191249bbd95c5
adam adifam <adamabdi.al.a@gmail.com>
2024-02-22 21:52:35 +0700

    update

--- resources/views/kasbesar/laporan/cetak_saldokasbesar.blade.php
+++ resources/views/kasbesar/laporan/cetak_saldokasbesar.blade.php
@@ -298,7 +298,7 @@
                 <td style="text-align:right; color:red; font-weight:bold">
                     {{ !empty($totalsetoranbank) ? rupiah($totalsetoranbank) : '' }}</td>
                 <td style="text-align:right; color:red; font-weight:bold">
-                    {{ !empty($totalsetoranlainnya) ? rupiah($totalsetoranlainnya) : '' }}</td>
+                    {{ !empty($setoranbank_lainnya) ? rupiah($setoranbank_lainnya) : '' }}</td>
                 <td style="text-align:right; color:rgb(3, 19, 241); font-weight:bold">
                     {{ !empty($saldo) ? rupiah($saldo) : '' }}</td>
                 <td style="border:none; background-color:white;"></td>
