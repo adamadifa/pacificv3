44985ec6fc485256aae1dd1b8498acfa35e9d942
Adam Adifa <adamabdi.al.a@gmail.com>
2024-02-27 12:54:50 +0700

    update

--- app/Http/Controllers/LaporankeuanganController.php
+++ app/Http/Controllers/LaporankeuanganController.php
@@ -117,7 +117,20 @@ class LaporankeuanganController extends Controller
                     $list = [
                         'BNI CV', 'BNI TGL', 'BNI SKB', 'BNI SBY', 'BNI PWT', 'BNI PWK', 'BNI KLT', 'BNI BTN', 'BNI BGR', 'BNI SMR', 'BNI BDG', 'BNI BKI',
                         'BNI GRT', '001', '002', '003', '004', '005', '006', '007', '008', '009', '010', '011', '012', '013', '014', '015', '016', '017', '018', '019', '020',
-                        '021', '022', '023', '024', '025', '027', '028'
+                        '021', '022', '023', '024', '025', '027', '028', '029',
+                        '030',
+                        '031',
+                        '032',
+                        '033',
+                        '034',
+                        '035',
+                        '036',
+                        '037',
+                        '038',
+                        '039',
+                        '040',
+                        '041',
+                        '042'
                     ];
                     $bank = DB::table('master_bank')->whereIn('kode_bank', $list)->orderBy('kode_bank')->get();
                 } else {
