a301ed4379e215bf03df9894a1a718e4802411a3
adam adifam <adamabdi.al.a@gmail.com>
2024-02-29 20:05:30 +0700

    update

--- app/Http/Controllers/TargetkomisiController.php
+++ app/Http/Controllers/TargetkomisiController.php
@@ -5484,7 +5484,7 @@ class TargetkomisiController extends Controller
 
         $query->where('kode_cabang', $cabang);
         $query->where('nama_karyawan', '!=', '');
-
+        $query->where('karyawan.id_karyawan', '!=', 'SKLT09');
         $komisi = $query->get();
         $nmbulan  = $namabulan[$bulan];
         if (isset($_POST['export'])) {
