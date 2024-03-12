e8aed34b039a2a35a5c2d6e35f2718de74da86f0
Adam Adifa <adamabdi.al.a@gmail.com>
2024-03-12 13:49:18 +0700

    update

--- app/Http/Controllers/LogaktivitasController.php
+++ app/Http/Controllers/LogaktivitasController.php
@@ -11,11 +11,13 @@ class LogaktivitasController extends Controller
     public function index(Request $request)
     {
 
+        $dari = !empty($request->dari) ? $request->dari : date('Y-m-d');
+        $sampai = !empty($request->sampai) ? $request->sampai : date('Y-m-d');
         $query = Logaktivitas::query();
         $query->join('users', 'log_aktivitas.id_user', '=', 'users.id');
         $query->where('id_user', $request->id_user);
-        $query->whereRaw('DATE(datetime)>="' . $request->dari . '"');
-        $query->whereRaw('DATE(datetime)<="' . $request->sampai . '"');
+        $query->whereRaw('DATE(datetime)>="' . $dari . '"');
+        $query->whereRaw('DATE(datetime)<="' . $sampai . '"');
         $log = $query->get();
         $cabang = Cabang::orderBy('nama_cabang')->get();
         return view('logaktivitas.index', compact('cabang', 'log'));
