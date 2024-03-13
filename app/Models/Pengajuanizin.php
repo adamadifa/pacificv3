31870c79614616e4ebe37fed29a03e412cb48124
adam adifam <adamabdi.al.a@gmail.com>
2024-03-13 10:51:24 +0700

    update

--- app/Models/Pengajuanizin.php
+++ app/Models/Pengajuanizin.php
@@ -345,11 +345,12 @@ class Pengajuanizin extends Model
         }
 
         if ($level == "kepala penjualan") {
-            if (Auth::user()->id == "27") {
-                $query->whereIn('master_karyawan.id_kantor', [$cabang, 'PWK']);
-            } else {
-                $query->where('master_karyawan.id_kantor', $cabang);
-            }
+            // if (Auth::user()->id == "27") {
+            //     $query->whereIn('master_karyawan.id_kantor', [$cabang, 'PWK']);
+            // } else {
+            //     $query->where('master_karyawan.id_kantor', $cabang);
+            // }
+            $query->where('master_karyawan.id_kantor', $cabang);
             $query->where('nama_jabatan', '!=', 'KEPALA PENJUALAN');
             $query->where('master_karyawan.id_perusahaan', "PCF");
         }
