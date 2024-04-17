3b1315bece6c10b415a00dd9ac12a1a5c954ed5b
Adam Adifa <adamabdi.al.a@gmail.com>
2024-04-17 13:56:27 +0700

    update

--- resources/views/kontrak/cetak.blade.php
+++ resources/views/kontrak/cetak.blade.php
@@ -173,7 +173,7 @@
             @else
                 {{ $cabang->nama_pt }}
             @endif
-            berkedudukan di {{$cabang->kode_cabang !="PST" ?  $cabang->nama_cabang : "TASIKMALAYA"  }} selanjutnya disebut <b>pihak kesatu.</b>
+            berkedudukan di {{$cabang->kode_cabang !="PST" ?  $cabang->nama_cabang : "Tasikmalaya"  }} selanjutnya disebut <b>pihak kesatu.</b>
         </p>
 
         <p>
@@ -267,7 +267,7 @@
                 @else
                     Perusahaan dengan Jabatan sebagai {{ $kontrak->nama_jabatan }}
                 @endif
-                yang berlokasi di {{ $cabang->kode_cabang != "PST" ?  $cabang->nama_cabang : "TASIKMALAYA" }}
+                yang berlokasi di {{ $cabang->kode_cabang != "PST" ?  $cabang->nama_cabang : "Tasikmalaya" }}
                 serta bersedia ditempatkan diluar lokasi dan departemen tersebut bila Perusahaan memerlukan.
             </li>
             <li>
