e67eadd6a69f91a1f1870c96c702c9521a8ebba1
Adam Adifa <adamabdi.al.a@gmail.com>
2024-05-06 14:01:27 +0700

    update

--- resources/views/targetkomisi/index.blade.php
+++ resources/views/targetkomisi/index.blade.php
@@ -119,7 +119,7 @@
                                             <td>
 
                                                 <div class="btn-group" role="group" aria-label="Basic example">
-                                                    @if ($d->bulan == 4 && $d->tahun == 2024)
+                                                    @if ($d->bulan == 5 && $d->tahun == 2024)
                                                         @if (in_array($level, $targetkomisiinput))
                                                             <a href="#" class="ml-1 btn btn-primary btn-sm settarget"
                                                                 kodetarget="{{ $d->kode_target }}"><i
