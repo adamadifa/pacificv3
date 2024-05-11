cd6a573dba3e2c0279fbc18f7feb64ee02bbc5fc
Adam Adifa <adamabdi.al.a@gmail.com>
2024-05-11 10:41:19 +0700

    update

--- resources/views/pembelian/prosespembelian.blade.php
+++ resources/views/pembelian/prosespembelian.blade.php
@@ -1,4 +1,5 @@
-<form action="/pembelian/{{ Crypt::encrypt($pembelian->nobukti_pembelian) }}/storeprosespembelian" method="POST" id="frmProsespembelian">
+<form action="/pembelian/{{ Crypt::encrypt($pembelian->nobukti_pembelian) }}/storeprosespembelian" method="POST"
+    id="frmProsespembelian">
     @csrf
     <table class="table table-bordered">
         <tr>
@@ -11,7 +12,7 @@
         </tr>
         <tr>
             <td>Supplier</td>
-            <td>{{ $pembelian->kode_supplier  }} - {{ $pembelian->nama_supplier }}</td>
+            <td>{{ $pembelian->kode_supplier }} - {{ $pembelian->nama_supplier }}</td>
         </tr>
         <tr>
             <td>Departemen</td>
@@ -21,7 +22,7 @@
             <td>PPN</td>
             <td class="success">
                 @if (!empty($pembelian->ppn))
-                <i class="fa fa-check"></i> {{ $pembelian->no_fak_pajak }}
+                    <i class="fa fa-check"></i> {{ $pembelian->no_fak_pajak }}
                 @endif
             </td>
         </tr>
@@ -37,37 +38,43 @@
                 <th>Kode Akun</th>
                 <th>Keterangan</th>
                 <th>Qty</th>
-                <th>Harga</th>
-                <th>Subtotal</th>
-                <th>Penyesuaian</th>
-                <th>Total</th>
+                @if (Auth::user()->level != 'admin gudang logistik')
+                    <th>Harga</th>
+                    <th>Subtotal</th>
+                    <th>Penyesuaian</th>
+                    <th>Total</th>
+                @endif
             </tr>
         </thead>
         <tbody>
             @php
-            $totalpembelian = 0;
+                $totalpembelian = 0;
             @endphp
             @foreach ($detailpembelian as $d)
-            @php
-            $total = ($d->qty * $d->harga) + $d->penyesuaian;
-            $totalpembelian += $total;
-            @endphp
-            <tr>
-                <td>{{ $d->kode_barang }}</td>
-                <td>{{ $d->nama_barang }}</td>
-                <td>{{ $d->kode_akun }} - {{ $d->nama_akun }}</td>
-                <td>{{ $d->keterangan }}</td>
-                <td class="text-center">{{ desimal($d->qty) }}</td>
-                <td class="text-right">{{ desimal($d->harga) }}</td>
-                <td class="text-right">{{ desimal($d->harga * $d->qty) }}</td>
-                <td class="text-right">{{ desimal($d->penyesuaian) }}</td>
-                <td class="text-right">{{ desimal($total) }}</td>
-            </tr>
+                @php
+                    $total = $d->qty * $d->harga + $d->penyesuaian;
+                    $totalpembelian += $total;
+                @endphp
+                <tr>
+                    <td>{{ $d->kode_barang }}</td>
+                    <td>{{ $d->nama_barang }}</td>
+                    <td>{{ $d->kode_akun }} - {{ $d->nama_akun }}</td>
+                    <td>{{ $d->keterangan }}</td>
+                    <td class="text-center">{{ desimal($d->qty) }}</td>
+                    @if (Auth::user()->level != 'admin gudang logistik')
+                        <td class="text-right">{{ desimal($d->harga) }}</td>
+                        <td class="text-right">{{ desimal($d->harga * $d->qty) }}</td>
+                        <td class="text-right">{{ desimal($d->penyesuaian) }}</td>
+                        <td class="text-right">{{ desimal($total) }}</td>
+                    @endif
+                </tr>
             @endforeach
-            <tr class="thead-dark">
-                <th colspan="8">TOTAL</th>
-                <th class="text-righ">{{ desimal($totalpembelian) }}</th>
-            </tr>
+            @if (Auth::user()->level != 'admin gudang logistik')
+                <tr class="thead-dark">
+                    <th colspan="8">TOTAL</th>
+                    <th class="text-righ">{{ desimal($totalpembelian) }}</th>
+                </tr>
+            @endif
         </tbody>
     </table>
     <input type="hidden" name="nobukti_pembelian" value="{{ $pembelian->nobukti_pembelian }}">
@@ -84,7 +91,7 @@
         </div>
     </div>
 </form>
-<script src="{{asset('app-assets/js/scripts/pickers/dateTime/pick-a-datetime.js')}}"></script>
+<script src="{{ asset('app-assets/js/scripts/pickers/dateTime/pick-a-datetime.js') }}"></script>
 <script>
     $(function() {
         $("#frmProsespembelian").submit(function() {
@@ -93,20 +100,20 @@
             var cektutuplaporan = $("#cektutuplaporan").val();
             if (cektutuplaporan == 1) {
                 swal({
-                    title: 'Oops'
-                    , text: 'Laporan Sudah Di Tutup !'
-                    , icon: 'warning'
-                    , showConfirmButton: false
+                    title: 'Oops',
+                    text: 'Laporan Sudah Di Tutup !',
+                    icon: 'warning',
+                    showConfirmButton: false
                 }).then(function() {
                     $("#tgl_pemasukan").focus();
                 });
                 return false;
             } else if (tgl_pemasukan == "") {
                 swal({
-                    title: 'Oops'
-                    , text: 'Tanggal Pemasukan Harus Diisi !'
-                    , icon: 'warning'
-                    , showConfirmButton: false
+                    title: 'Oops',
+                    text: 'Tanggal Pemasukan Harus Diisi !',
+                    icon: 'warning',
+                    showConfirmButton: false
                 }).then(function() {
                     $("#tgl_pemasukan").focus();
                 });
@@ -116,15 +123,15 @@
 
         function cektutuplaporan(tanggal) {
             $.ajax({
-                type: "POST"
-                , url: "/cektutuplaporan"
-                , data: {
-                    _token: "{{ csrf_token() }}"
-                    , tanggal: tanggal
-                    , jenislaporan: "gudanglogistik"
-                }
-                , cache: false
-                , success: function(respond) {
+                type: "POST",
+                url: "/cektutuplaporan",
+                data: {
+                    _token: "{{ csrf_token() }}",
+                    tanggal: tanggal,
+                    jenislaporan: "gudanglogistik"
+                },
+                cache: false,
+                success: function(respond) {
                     console.log(respond);
                     $("#cektutuplaporan").val(respond);
                 }
@@ -138,5 +145,4 @@
 
 
     });
-
 </script>
