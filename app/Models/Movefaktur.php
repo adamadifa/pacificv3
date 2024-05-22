5b51c080ca9c1d4ff2c7e9e08e61dca09765b3a1
adam adifam <adamabdi.al.a@gmail.com>
2024-05-22 08:24:37 +0700

    update

---
+++ app/Models/Movefaktur.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Movefaktur extends Model
+{
+    use HasFactory;
+}
