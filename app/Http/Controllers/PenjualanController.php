26d0e50b329e672239e1cdcb077d8f82ca6523f1
Adam Adifa <adamabdi.al.a@gmail.com>
2024-05-25 07:54:25 +0700

    update

--- app/Http/Controllers/PenjualanController.php
+++ app/Http/Controllers/PenjualanController.php
@@ -9625,7 +9625,7 @@ class PenjualanController extends Controller
             $query->selectRaw(
                 'penjualan.id_karyawan,nama_karyawan,
                 SUM(IF(penjualan.signature IS NOT NULL,1,0)) sudahada,
-                SUM(IF(penjualan.signature NULL,1,0)) belumada'
+                SUM(IF(penjualan.signature IS NULL,1,0)) belumada'
             );
             $query->join('karyawan', 'penjualan.id_karyawan', '=', 'karyawan.id_karyawan');
             $query->join('pelanggan', 'penjualan.kode_pelanggan', '=', 'pelanggan.kode_pelanggan');
