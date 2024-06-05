4815075e3a82a5421d498e9ba2a5de24249c58fe
Adam Adifa <adamabdi.al.a@gmail.com>
2024-06-05 10:43:44 +0700

    updatE

--- app/Http/Controllers/TargetkomisiController.php
+++ app/Http/Controllers/TargetkomisiController.php
@@ -5539,8 +5539,9 @@ class TargetkomisiController extends Controller
 
 
         //dd($lastdateofmonth);
-        $last3month = date('Y-m-d', strtotime('-4 month', strtotime($sampai)));
+        $last3month = date('Y-m-d', strtotime('-3 month', strtotime($sampai)));
         $date = explode("-", $last3month);
+        dd($last3month);
         $startdate = $date[0] . "-" . $date[1] . "-01";
 
         dd($startdate . "-" . $enddate);
