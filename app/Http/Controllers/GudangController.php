d209a8badb8074dd50c0ea6ca81814b405de0233
adam adifam <adamabdi.al.a@gmail.com>
2024-06-10 01:09:13 +0700

    update

--- app/Http/Controllers/GudangController.php
+++ app/Http/Controllers/GudangController.php
@@ -36,7 +36,8 @@ class GudangController extends Controller
         $lastsaldo = DB::select("SELECT tanggal, kode_cabang FROM (SELECT MAX(tanggal) as tanggal,kode_cabang FROM saldoawal_bj GROUP BY kode_cabang ) as saldobj WHERE kode_cabang != 'PST' ORDER BY tanggal LIMIT 1");
         // (SELECT MAX(saldomax.tanggal) FROM saldoawal_bj saldomax
         //     WHERE saldomax.kode_cabang = mc.kode_cabang)
-        dd($lastsaldo[0]->tanggal);
+        $lastdate = $lastsaldo[0]->tanggal;
+
         $barang = DB::table('master_barang')
             ->where('status', 1)
             ->orderBy('kode_produk')
@@ -106,22 +107,22 @@ class GudangController extends Controller
             WHERE saldomax.kode_cabang = mc.kode_cabang)
             AND tgl_mutasi_gudang_cabang <= CURDATE()
             AND jenis_mutasi='SURAT JALAN'
-            OR tgl_mutasi_gudang_cabang >= '$lastsaldo->tanggal'
+            OR tgl_mutasi_gudang_cabang >= '$lastdate'
             AND tgl_mutasi_gudang_cabang <= CURDATE()
             AND jenis_mutasi='TRANSIT IN'
-            OR tgl_mutasi_gudang_cabang>= '$lastsaldo->tanggal'
+            OR tgl_mutasi_gudang_cabang>= '$lastdate'
             AND tgl_mutasi_gudang_cabang <= CURDATE()
             AND jenis_mutasi='TRANSIT OUT'
-            OR tgl_mutasi_gudang_cabang>= '$lastsaldo->tanggal'
+            OR tgl_mutasi_gudang_cabang>= '$lastdate'
             AND tgl_mutasi_gudang_cabang <= CURDATE()
             AND jenis_mutasi='REJECT GUDANG'
-            OR tgl_mutasi_gudang_cabang>= '$lastsaldo->tanggal'
+            OR tgl_mutasi_gudang_cabang>= '$lastdate'
             AND tgl_mutasi_gudang_cabang <= CURDATE()
             AND jenis_mutasi='REJECT PASAR'
-            OR tgl_mutasi_gudang_cabang >= '$lastsaldo->tanggal'
+            OR tgl_mutasi_gudang_cabang >= '$lastdate'
             AND tgl_mutasi_gudang_cabang <= CURDATE()
             AND jenis_mutasi='REPACK'
-            OR tgl_mutasi_gudang_cabang>= '$lastsaldo->tanggal'
+            OR tgl_mutasi_gudang_cabang>= '$lastdate'
             AND tgl_mutasi_gudang_cabang <= CURDATE()
             AND jenis_mutasi='PENYESUAIAN'
             GROUP BY kode_cabang
@@ -138,7 +139,7 @@ class GudangController extends Controller
             kode_cabang
             FROM detail_dpb
             INNER JOIN dpb ON detail_dpb.no_dpb = dpb.no_dpb
-            WHERE tgl_pengambilan >= $lastsaldo->tanggal AND tgl_pengambilan <= CURDATE()
+            WHERE tgl_pengambilan >= $lastdate AND tgl_pengambilan <= CURDATE()
             GROUP BY kode_cabang
             ) dpb"),
             function ($join) {
