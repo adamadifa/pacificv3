ee4c719d7f4175ea43e1246707bc98a8682bf574
Adam Adifa <adamabdi.al.a@gmail.com>
2024-06-15 09:03:27 +0700

    update

--- app/Http/Controllers/KaryawanController.php
+++ app/Http/Controllers/KaryawanController.php
@@ -64,11 +64,12 @@ class KaryawanController extends Controller
         }
 
         if ($level == "kepala penjualan") {
-            if (Auth::user()->id == "27") {
-                $query->whereIn('master_karyawan.id_kantor', [$cabang, 'PWK']);
-            } else {
-                $query->where('master_karyawan.id_kantor', $cabang);
-            }
+            // if (Auth::user()->id == "27") {
+            //     $query->whereIn('master_karyawan.id_kantor', [$cabang, 'PWK']);
+            // } else {
+            //     $query->where('master_karyawan.id_kantor', $cabang);
+            // }
+            $query->where('master_karyawan.id_kantor', $cabang);
             $query->where('nama_jabatan', '!=', 'KEPALA PENJUALAN');
             $query->where('master_karyawan.id_perusahaan', "PCF");
         }
