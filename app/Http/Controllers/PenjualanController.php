aa82cb367215b58735c12ef2794bddc681af82db
Adam Adifa <adamabdi.al.a@gmail.com>
2024-08-03 08:10:06 +0700

    update

--- app/Http/Controllers/PenjualanController.php
+++ app/Http/Controllers/PenjualanController.php
@@ -106,13 +106,17 @@ class PenjualanController extends Controller
                     INNER JOIN karyawan ON pj.id_karyawan = karyawan.id_karyawan
                     LEFT JOIN (
                     SELECT
+                        MAX( id_move ) AS id_move,
                         no_fak_penj,
                         move_faktur.id_karyawan AS salesbaru,
                         karyawan.kode_cabang AS cabangbaru
                     FROM
                         move_faktur
                         INNER JOIN karyawan ON move_faktur.id_karyawan = karyawan.id_karyawan
-                    WHERE id IN (SELECT MAX(id) FROM move_faktur GROUP BY no_fak_penj)
+                    GROUP BY
+                        no_fak_penj,
+                        move_faktur.id_karyawan,
+                        karyawan.kode_cabang
                     ) move_fak ON ( pj.no_fak_penj = move_fak.no_fak_penj)
                 ) pjmove"),
                 function ($join) {
