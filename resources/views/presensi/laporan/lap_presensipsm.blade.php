7d4c015b0d05ccb35ba2155a7d76254ea333f9b8
Adam Adifa <adamabdi.al.a@gmail.com>
2024-08-09 09:11:44 +0700

    update

--- resources/views/presensi/laporan/lap_presensipsm.blade.php
+++ resources/views/presensi/laporan/lap_presensipsm.blade.php
@@ -27,8 +27,7 @@
                         <div class="col-lg-7 col-sm-12">
                             <div class="card">
                                 <div class="card-body">
-                                    <form action="/laporanhrd/presensipsm/cetak" method="POST" id="frmPresensi"
-                                        target="_blank">
+                                    <form action="/laporanhrd/presensipsm/cetak" method="POST" id="frmPresensi" target="_blank">
                                         @csrf
 
                                         <div class="row">
@@ -43,8 +42,7 @@
                                                             <option value="">Semua Kantor</option>
                                                         @endif
                                                         @foreach ($cabang as $c)
-                                                            <option
-                                                                {{ Request('kode_cabang') == $c->kode_cabang ? 'selected' : '' }}
+                                                            <option {{ Request('kode_cabang') == $c->kode_cabang ? 'selected' : '' }}
                                                                 value="{{ $c->kode_cabang }}">
                                                                 {{ strtoupper($c->kode_cabang == 'PST' ? 'PUSAT' : $c->nama_cabang) }}
                                                             </option>
@@ -111,19 +109,28 @@
                                                 </div>
                                             </div>
                                         </div>
-
+                                        <div class="row">
+                                            <div class="col-12">
+                                                <div class="form-group">
+                                                    <select name="jenis_laporan" required id="jenis_laporan" class="form-control">
+                                                        <option value="">Jenis Laporan</option>
+                                                        <option value="1">Periode Gaji</option>
+                                                        <option value="2">Periode Bulan Berjalan</option>
+                                                    </select>
+                                                </div>
+                                            </div>
+                                        </div>
                                         <div class="row">
                                             <div class="col-lg-8 col-sm-12">
                                                 <div class="form-group">
-                                                    <button type="submit" name="submit"
-                                                        class="btn btn-primary btn-block"><i class="feather icon-send"></i>
+                                                    <button type="submit" name="submit" class="btn btn-primary btn-block"><i
+                                                            class="feather icon-send"></i>
                                                         Submit</button>
                                                 </div>
                                             </div>
                                             <div class="col-lg-4 col-sm-12">
                                                 <div class="form-group">
-                                                    <button type="submit" name="export"
-                                                        class="btn btn-success btn-block"><i
+                                                    <button type="submit" name="export" class="btn btn-success btn-block"><i
                                                             class="feather icon-download"></i> Export</button>
                                                 </div>
                                             </div>
