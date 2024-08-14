3b3ce97a7d67cc97a0ac3b2a01b4377c6c6ef7c0
adam adifam <adamabdi.al.a@gmail.com>
2024-08-14 20:01:19 +0700

    update

--- resources/views/retur/showbarangtempv2.blade.php
+++ resources/views/retur/showbarangtempv2.blade.php
@@ -20,47 +20,47 @@
         </thead>
         <tbody>
             @php
-            $total = 0;
+                $total = 0;
             @endphp
             @foreach ($detailtemp as $d)
-            @php
-            $isipcsdus = $d->isipcsdus;
-            $isipack = $d->isipack;
-            $isipcs = $d->isipcs;
-            $jumlah = $d->jumlah;
-            $jumlah_dus = floor($jumlah / $isipcsdus);
-            if ($jumlah != 0) {
-            $sisadus = $jumlah % $isipcsdus;
-            } else {
-            $sisadus = 0;
-            }
-            if ($isipack == 0) {
-            $jumlah_pack = 0;
-            $sisapack = $sisadus;
-            } else {
-            $jumlah_pack = floor($sisadus / $isipcs);
-            $sisapack = $sisadus % $isipcs;
-            }
+                @php
+                    $isipcsdus = $d->isipcsdus;
+                    $isipack = $d->isipack;
+                    $isipcs = $d->isipcs;
+                    $jumlah = $d->jumlah;
+                    $jumlah_dus = floor($jumlah / $isipcsdus);
+                    if ($jumlah != 0) {
+                        $sisadus = $jumlah % $isipcsdus;
+                    } else {
+                        $sisadus = 0;
+                    }
+                    if ($isipack == 0) {
+                        $jumlah_pack = 0;
+                        $sisapack = $sisadus;
+                    } else {
+                        $jumlah_pack = floor($sisadus / $isipcs);
+                        $sisapack = $sisadus % $isipcs;
+                    }
 
-            $jumlah_pcs = $sisapack;
-            $total += $d->subtotal;
-            @endphp
-            <tr>
-                <td>{{ $loop->iteration }}</td>
-                <td>{{ $d->kode_barang }}</td>
-                <td>{{ $d->nama_barang }}</td>
-                <td class="text-center">{{ !empty($jumlah_dus) ? rupiah($jumlah_dus) : '' }}</td>
-                <td class="text-right">{{ rupiah($d->harga_dus) }}</td>
-                <td class="text-center">{{ !empty($jumlah_pack) ? rupiah($jumlah_pack) : '' }}</td>
-                <td class="text-right">{{ rupiah($d->harga_pack)  }}</td>
-                <td class="text-center">{{ !empty($jumlah_pcs) ? rupiah($jumlah_pcs) : '' }}</td>
-                <td class="text-right">{{ rupiah($d->harga_pcs) }}</td>
-                <td class="text-right">{{ !empty($d->subtotal) ? rupiah($d->subtotal) : '' }}</td>
-                <td>
-                    {{-- <a href="#" class="info edit" kode_barang="{{ $d->kode_barang }}"><i class=" feather icon-edit"></i></a> --}}
-                    <a href="#" class="danger hapus" kode_barang="{{ $d->kode_barang }}"><i class=" feather icon-trash"></i></a>
-                </td>
-            </tr>
+                    $jumlah_pcs = $sisapack;
+                    $total += $d->subtotal;
+                @endphp
+                <tr>
+                    <td>{{ $loop->iteration }}</td>
+                    <td>{{ $d->kode_barang }}</td>
+                    <td>{{ $d->nama_barang }}</td>
+                    <td class="text-center">{{ !empty($jumlah_dus) ? rupiah($jumlah_dus) : '' }}</td>
+                    <td class="text-right">{{ rupiah($d->harga_dus) }}</td>
+                    <td class="text-center">{{ !empty($jumlah_pack) ? rupiah($jumlah_pack) : '' }}</td>
+                    <td class="text-right">{{ rupiah($d->harga_pack) }}</td>
+                    <td class="text-center">{{ !empty($jumlah_pcs) ? rupiah($jumlah_pcs) : '' }}</td>
+                    <td class="text-right">{{ rupiah($d->harga_pcs) }}</td>
+                    <td class="text-right">{{ !empty($d->subtotal) ? rupiah($d->subtotal) : '' }}</td>
+                    <td>
+                        {{-- <a href="#" class="info edit" kode_barang="{{ $d->kode_barang }}"><i class=" feather icon-edit"></i></a> --}}
+                        <a href="#" class="danger hapus" kode_barang="{{ $d->kode_barang }}"><i class=" feather icon-trash"></i></a>
+                    </td>
+                </tr>
             @endforeach
             <tr style="font-weight: bold">
                 <td colspan="9">TOTAL</td>
@@ -77,14 +77,14 @@
             function loadbarangtemp() {
                 var kode_pelanggan = $("#kode_pelanggan").val();
                 $.ajax({
-                    type: 'POST'
-                    , url: '/retur/showbarangtempv2'
-                    , data: {
-                        _token: "{{ csrf_token() }}"
-                        , kode_pelanggan: kode_pelanggan
-                    , }
-                    , cache: false
-                    , success: function(respond) {
+                    type: 'POST',
+                    url: '/retur/showbarangtempv2',
+                    data: {
+                        _token: "{{ csrf_token() }}",
+                        kode_pelanggan: kode_pelanggan,
+                    },
+                    cache: false,
+                    success: function(respond) {
                         $("#loadbarangtemp").html(respond);
                         loadtotal();
                     }
@@ -94,13 +94,13 @@
             function loadtotal() {
                 var kode_pelanggan = $("#kode_pelanggan").val();
                 $.ajax({
-                    type: 'POST'
-                    , url: '/loadtotalreturtemp'
-                    , data: {
-                        _token: "{{ csrf_token() }}"
-                        , kode_pelanggan: kode_pelanggan
-                    }
-                    , success: function(respond) {
+                    type: 'POST',
+                    url: '/loadtotalreturtemp',
+                    data: {
+                        _token: "{{ csrf_token() }}",
+                        kode_pelanggan: kode_pelanggan
+                    },
+                    success: function(respond) {
                         var total = parseInt(respond.replace(/\./g, ''));
                         $("#grandtotal").text(convertToRupiah(total));
                         // $("#total").val(convertToRupiah(grandtotal));
@@ -114,13 +114,13 @@
             function cektemp() {
                 var kode_pelanggan = $("#kode_pelanggan").val();
                 $.ajax({
-                    type: 'POST'
-                    , url: '/cekreturtemp'
-                    , data: {
-                        _token: "{{ csrf_token() }}"
-                        , kode_pelanggan: kode_pelanggan
-                    }
-                    , success: function(respond) {
+                    type: 'POST',
+                    url: '/cekreturtemp',
+                    data: {
+                        _token: "{{ csrf_token() }}",
+                        kode_pelanggan: kode_pelanggan
+                    },
+                    success: function(respond) {
                         $("#cektemp").val(respond);
                     }
                 });
@@ -153,28 +153,26 @@
                 var kode_pelanggan = $("#kode_pelanggan").val();
                 event.preventDefault();
                 swal({
-                        title: `Anda Yakin Data ini Akan Dihapus ?`
-                        , text: "Jika dihapus Data Ini Akan Hilang Dari Keranjang"
-                        , icon: "warning"
-                        , buttons: true
-                        , dangerMode: true
-                    , })
+                        title: `Anda Yakin Data ini Akan Dihapus ?`,
+                        text: "Jika dihapus Data Ini Akan Hilang Dari Keranjang",
+                        icon: "warning",
+                        buttons: true,
+                        dangerMode: true,
+                    })
                     .then((willDelete) => {
                         if (willDelete) {
                             $.ajax({
-                                type: 'POST'
-                                , url: '/retur/deletebarangtemp'
-                                , data: {
-                                    _token: "{{ csrf_token() }}"
-                                    , kode_barang: kode_barang
-                                    , kode_pelanggan: kode_pelanggan
-                                }
-                                , cache: false
-                                , success: function(respond) {
+                                type: 'POST',
+                                url: '/retur/deletebarangtemp',
+                                data: {
+                                    _token: "{{ csrf_token() }}",
+                                    kode_barang: kode_barang,
+                                    kode_pelanggan: kode_pelanggan
+                                },
+                                cache: false,
+                                success: function(respond) {
                                     swal(
-                                        'Deleted!'
-                                        , 'Data Berhasil Dihapus'
-                                        , 'success'
+                                        'Deleted!', 'Data Berhasil Dihapus', 'success'
                                     )
                                     loadbarangtemp();
                                 }
@@ -183,5 +181,4 @@
                     });
             });
         });
-
     </script>
