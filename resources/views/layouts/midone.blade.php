8fdd6a855fbf4143c80d1457fb499a983c71657c
adam adifam <adamabdi.al.a@gmail.com>
2024-09-06 05:43:20 +0700

    update

--- resources/views/layouts/midone.blade.php
+++ resources/views/layouts/midone.blade.php
@@ -8,8 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description"
         content="Vuexy admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities.">
-    <meta name="keywords"
-        content="admin template, Vuexy admin template, dashboard template, flat admin template, responsive admin template, web app">
+    <meta name="keywords" content="admin template, Vuexy admin template, dashboard template, flat admin template, responsive admin template, web app">
     <meta name="author" content="PIXINVENT">
     <title>@yield('titlepage')</title>
     @include('layouts.style')
@@ -28,8 +27,8 @@
 
 @endphp
 
-<body class="vertical-layout vertical-menu-modern 2-columns {{ $tema }}  navbar-floating footer-static"
-    data-open="click" data-menu="vertical-menu-modern" data-col="2-columns">
+<body class="vertical-layout vertical-menu-modern 2-columns {{ $tema }}  navbar-floating footer-static" data-open="click"
+    data-menu="vertical-menu-modern" data-col="2-columns">
 
     <div class="loader-wrapper">
         <span class="loader"><span class="loader-inner"></span></span>
@@ -45,10 +44,25 @@
     <div class="app-content content">
         <div class="content-overlay"></div>
         <div class="header-navbar-shadow"></div>
+        <div class="alert alert-warning">
+            <p>Dikarenakan Sedang Proses Migrasi dari v3 ke v4, dan mengingat proses migrasi membutuhkan waktu yang cukup lama, maka dari itu akan
+                dilakukan proses migrasi secara bertahap, di mulai dari tanggal 05 September 2024
+                Sehingga Beberapa Menu akan di Non Aktifkan secara bertaap, dan Saat Ini Menu Yang di Nonaktifkan Antara Lain,
+            <ul>
+                <li>Produksi</li>
+                <li>Gudang Bahan</li>
+                <li>Gudang Logistik</li>
+                <li>Gudang jadi Pusat</li>
+                <li>Pembelian</li>
+                <li>Keuangan</li>
+            </ul>
+            dan perkiraan aktif kembali pada hari Senin, 08 September 2024
+            Atas Perhatiannya Kami Ucapkan Terima Kasih !
+            </p>
+        </div>
         @yield('content')
         <!-- BEGIN: Customizer-->
-        <div class="customizer d-none d-md-block"><a
-                class="customizer-toggle d-flex align-items-center justify-content-center" href="#"><i
+        <div class="customizer d-none d-md-block"><a class="customizer-toggle d-flex align-items-center justify-content-center" href="#"><i
                     class="spinner-grow white"></i></a>
             <div class="customizer-content">
                 <!-- Customizer header -->
@@ -70,8 +84,7 @@
                                     @endphp
                                     <img src="{{ url($path) }}" alt="avtar img holder" height="35" width="35">
                                 @else
-                                    <img src="{{ asset('app-assets/images/avatar.png') }}" alt="avtar img holder"
-                                        height="35" width="35">
+                                    <img src="{{ asset('app-assets/images/avatar.png') }}" alt="avtar img holder" height="35" width="35">
                                 @endif
                             </div>
                             <div class="user-page-info ml-1">
