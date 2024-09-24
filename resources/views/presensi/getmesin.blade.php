ab17ac41eb1f0e07492e10ca41fc5c04a3d78132
Adam Adifa <adamabdi.al.a@gmail.com>
2024-09-24 15:09:28 +0700

    update

--- resources/views/presensi/getmesin.blade.php
+++ resources/views/presensi/getmesin.blade.php
@@ -1,30 +1,35 @@
 <div class="row">
     <div class="col-12">
-        <table class="table">
-            <tr>
-                <th colspan="3">Mesin 1</th>
-            </tr>
-            <tr>
-                <th>PIN</th>
-                <th>Status Scan</th>
-                <th>Scan Date</th>
-                <th>#</th>
-            </tr>
+        <table class="table table-bordered">
+            <thead class="thead-dark">
+                <tr>
+                    <th colspan="3">Mesin 1</th>
+                </tr>
+                <tr>
+                    <th>PIN</th>
+                    <th>Status Scan</th>
+                    <th>Scan Date</th>
+                    <th>#</th>
+                </tr>
+            </thead>
+
             @foreach ($filtered_array as $d)
-            <tr>
-                <td>{{ $d->pin }}</td>
-                <td>{{ $d->status_scan % 2 == 0 ? "IN" : "OUT"}} ({{ $d->status_scan }})</td>
-                <td>{{ date("d-m-Y H:i:s",strtotime($d->scan_date)) }}</td>
-                <td>
-                    <div class="btn-group">
-                        <a href="/presensi/{{ Crypt::encrypt($d->pin) }}/0/{{ date("Y-m-d H:i:s",strtotime($d->scan_date)) }}/updatefrommachine"> <i class="feather icon-log-in success mr-1"></i> Masuk </a>
-                        <a href="/presensi/{{ Crypt::encrypt($d->pin) }}/1/{{ date("Y-m-d H:i:s",strtotime($d->scan_date)) }}/updatefrommachine" class="mr-1"> <i class="feather icon-log-out danger ml-1"></i> Pulang </a>
-                    </div>
+                <tr>
+                    <td>{{ $d->pin }}</td>
+                    <td>{{ $d->status_scan % 2 == 0 ? 'IN' : 'OUT' }} ({{ $d->status_scan }})</td>
+                    <td>{{ date('d-m-Y H:i:s', strtotime($d->scan_date)) }}</td>
+                    <td>
+                        <div class="btn-group">
+                            <a href="/presensi/{{ Crypt::encrypt($d->pin) }}/0/{{ date('Y-m-d H:i:s', strtotime($d->scan_date)) }}/updatefrommachine">
+                                <i class="feather icon-log-in success mr-1"></i> Masuk </a>
+                            <a href="/presensi/{{ Crypt::encrypt($d->pin) }}/1/{{ date('Y-m-d H:i:s', strtotime($d->scan_date)) }}/updatefrommachine"
+                                class="mr-1"> <i class="feather icon-log-out danger ml-1"></i> Pulang </a>
+                        </div>
 
 
 
-                </td>
-            </tr>
+                    </td>
+                </tr>
             @endforeach
         </table>
     </div>
@@ -43,17 +48,20 @@
                 <th>#</th>
             </tr>
             @foreach ($filtered_array_2 as $d)
-            <tr>
-                <td>{{ $d->pin }}</td>
-                <td>{{ $d->status_scan % 2 == 0 ? "IN" : "OUT"}} ({{ $d->status_scan }})</td>
-                <td>{{ date("d-m-Y H:i:s",strtotime($d->scan_date)) }}</td>
-                <td>
-                    <div class="btn-group">
-                        <a href="/presensi/{{ Crypt::encrypt($d->pin) }}/0/{{ date("Y-m-d H:i:s",strtotime($d->scan_date)) }}/updatefrommachine"> <i class="feather icon-log-in success mr-1"></i> Masuk </a>
-                        <a href="/presensi/{{ Crypt::encrypt($d->pin) }}/1/{{ date("Y-m-d H:i:s",strtotime($d->scan_date)) }}/updatefrommachine" class="ml-1"> <i class="feather icon-log-out danger mr-1"></i> Pulang </a>
-                    </div>
-                </td>
-            </tr>
+                <tr>
+                    <td>{{ $d->pin }}</td>
+                    <td>{{ $d->status_scan % 2 == 0 ? 'IN' : 'OUT' }} ({{ $d->status_scan }})</td>
+                    <td>{{ date('d-m-Y H:i:s', strtotime($d->scan_date)) }}</td>
+                    <td>
+                        <div class="btn-group">
+                            <a
+                                href="/presensi/{{ Crypt::encrypt($d->pin) }}/0/{{ date('Y-m-d H:i:s', strtotime($d->scan_date)) }}/updatefrommachine">
+                                <i class="feather icon-log-in success mr-1"></i> Masuk </a>
+                            <a href="/presensi/{{ Crypt::encrypt($d->pin) }}/1/{{ date('Y-m-d H:i:s', strtotime($d->scan_date)) }}/updatefrommachine"
+                                class="ml-1"> <i class="feather icon-log-out danger mr-1"></i> Pulang </a>
+                        </div>
+                    </td>
+                </tr>
             @endforeach
 
         </table>
--- resources/views/produksi/laporan/frm/lap_pengeluaranproduksi.blade.php
+++ resources/views/produksi/laporan/frm/lap_pengeluaranproduksi.blade.php
@@ -1,141 +1,142 @@
 @extends('layouts.midone')
-@section('titlepage','Laporan Pengeluaran Barang Produksi')
+@section('titlepage', 'Laporan Pengeluaran Barang Produksi')
 @section('content')
-<div class="content-wrapper">
-    <div class="content-header row">
-        <div class="content-header-left col-md-9 col-12 mb-2">
-            <div class="row breadcrumbs-top">
-                <div class="col-12">
-                    <h2 class="content-header-title float-left mb-0">Laporan Pengeluaran Barang Produksi</h2>
-                    <div class="breadcrumb-wrapper col-12">
-                        <ol class="breadcrumb">
-                            <li class="breadcrumb-item"><a href="/laporanproduksi/pemasukanproduksi">Laporan Pengeluaran Barang Produksi</a>
-                            </li>
-                        </ol>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="content-body">
-        <!-- Data list view starts -->
-        <!-- DataTable starts -->
-        @include('layouts.notification')
-        <div class="row">
-            <div class="col-lg-9 col-sm-12">
-                <div class="row">
-                    <div class="col-lg-7 col-sm-12">
-                        <div class="card">
-                            <div class="card-body">
-                                <form action="/laporanproduksi/pengeluaranproduksi/cetak" method="POST" id="frmMutasiproduksi" target="_blank">
-                                    @csrf
-                                    <div class="row">
-                                        <div class="col-12">
-                                            <div class="form-group">
-                                                <select name="kode_barang" id="kode_barang" class="form-control">
-                                                    <option value="">Semua Jenis Pengeluaran</option>
-                                                    <option value="Pemakaian">Pemakaian</option>
-                                                    <option value="Retur Out">Retur Out</option>
-                                                    <option value="Lainnya">Lainnya</option>
-                                                </select>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col-lg-12 col-sm-12">
-                                            <div class="form-group">
-                                                <select name="kode_barang" id="kode_barang" class="form-control select2">
-                                                    <option value="">Semua Barang</option>
-                                                    @foreach ($barang as $d)
-                                                    <option value="{{ $d->kode_barang }}">{{ $d->nama_barang }}</option>
-                                                    @endforeach
-                                                </select>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="row" id="pilihperiode">
-                                        <div class="col-6">
-                                            <x-inputtext label="Dari" field="dari" icon="feather icon-calendar" datepicker />
-                                        </div>
-                                        <div class="col-6">
-                                            <x-inputtext label="Sampai" field="sampai" icon="feather icon-calendar" datepicker />
-                                        </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col-lg-8 col-sm-12">
-                                            <div class="form-group">
-                                                <button type="submit" name="submit" class="btn btn-primary btn-block"><i class="feather icon-printer"></i> Cetak</button>
-                                            </div>
-                                        </div>
-                                        <div class="col-lg-4 col-sm-12">
-                                            <div class="form-group">
-                                                <button type="submit" name="export" class="btn btn-success btn-block"><i class="feather icon-download"></i> Export</button>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </form>
-                                <!-- DataTable ends -->
-                            </div>
+    <div class="content-wrapper">
+        <div class="content-header row">
+            <div class="content-header-left col-md-9 col-12 mb-2">
+                <div class="row breadcrumbs-top">
+                    <div class="col-12">
+                        <h2 class="content-header-title float-left mb-0">Laporan Pengeluaran Barang Produksi</h2>
+                        <div class="breadcrumb-wrapper col-12">
+                            <ol class="breadcrumb">
+                                <li class="breadcrumb-item"><a href="/laporanproduksi/pemasukanproduksi">Laporan Pengeluaran Barang Produksi</a>
+                                </li>
+                            </ol>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="col-lg-3 col-sm-12">
-                @include('layouts.nav_laporanproduksi')
-            </div>
-
-            <div class="col-lg-8 col-sm-12">
-
-
-            </div>
         </div>
-        <!-- Data list view end -->
+        <div class="content-body">
+            <!-- Data list view starts -->
+            <!-- DataTable starts -->
+            @include('layouts.notification')
+            <div class="row">
+                <div class="col-lg-9 col-sm-12">
+                    <div class="row">
+                        <div class="col-lg-7 col-sm-12">
+                            <div class="card">
+                                <div class="card-body">
+                                    <form action="/laporanproduksi/pengeluaranproduksi/cetak" method="POST" id="frmMutasiproduksi" target="_blank">
+                                        @csrf
+                                        <div class="row">
+                                            <div class="col-12">
+                                                <div class="form-group">
+                                                    <select name="kode_barang" id="kode_barang" class="form-control">
+                                                        <option value="">Semua Jenis Pengeluaran</option>
+                                                        <option value="Pemakaian">Pemakaian</option>
+                                                        <option value="Retur Out">Retur Out</option>
+                                                        <option value="Lainnya">Lainnya</option>
+                                                    </select>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-lg-12 col-sm-12">
+                                                <div class="form-group">
+                                                    <select name="kode_barang" id="kode_barang" class="form-control select2">
+                                                        <option value="">Semua Barang</option>
+                                                        @foreach ($barang as $d)
+                                                            <option value="{{ $d->kode_barang }}">{{ $d->nama_barang }}</option>
+                                                        @endforeach
+                                                    </select>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="row" id="pilihperiode">
+                                            <div class="col-6">
+                                                <x-inputtext label="Dari" field="dari" icon="feather icon-calendar" datepicker />
+                                            </div>
+                                            <div class="col-6">
+                                                <x-inputtext label="Sampai" field="sampai" icon="feather icon-calendar" datepicker />
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-lg-8 col-sm-12">
+                                                <div class="form-group">
+                                                    <button type="submit" name="submit" class="btn btn-primary btn-block"><i
+                                                            class="feather icon-printer"></i> Cetak</button>
+                                                </div>
+                                            </div>
+                                            <div class="col-lg-4 col-sm-12">
+                                                <div class="form-group">
+                                                    <button type="submit" name="export" class="btn btn-success btn-block"><i
+                                                            class="feather icon-download"></i> Export</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </form>
+                                    <!-- DataTable ends -->
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-12">
+                    @include('layouts.nav_laporanproduksi')
+                </div>
+
+                <div class="col-lg-8 col-sm-12">
+
+
+                </div>
+            </div>
+            <!-- Data list view end -->
+        </div>
     </div>
-</div>
 @endsection
 @push('myscript')
-<script>
-    $(function() {
-        $("#frmMutasiproduksi").submit(function() {
-            var dari = $("#dari").val();
-            var sampai = $("#sampai").val();
-            var start = new Date(dari);
-            var end = new Date(sampai);
-            if (dari == "" || sampai == "") {
-                swal({
-                    title: 'Oops'
-                    , text: 'Periode Harus Diisi !'
-                    , icon: 'warning'
-                    , showConfirmButton: false
-                }).then(function() {
-                    $("#dari").focus();
-                });
-                return false;
-            } else if (start.getTime() > end.getTime()) {
-                swal({
-                    title: 'Oops'
-                    , text: 'Periode tidak Valid !'
-                    , icon: 'warning'
-                    , showConfirmButton: false
-                }).then(function() {
-                    $("#dari").focus();
-                });
-                return false;
-            } else if (start.getTime() < datestart.getTime()) {
-                swal({
-                    title: 'Oops'
-                    , text: 'Periode tidak Valid !'
-                    , icon: 'warning'
-                    , showConfirmButton: false
-                }).then(function() {
-                    $("#dari").focus();
-                });
-                return false;
-            } else {
-                return true;
-            }
+    <script>
+        $(function() {
+            $("#frmMutasiproduksi").submit(function() {
+                var dari = $("#dari").val();
+                var sampai = $("#sampai").val();
+                var start = new Date(dari);
+                var end = new Date(sampai);
+                if (dari == "" || sampai == "") {
+                    swal({
+                        title: 'Oops',
+                        text: 'Periode Harus Diisi !',
+                        icon: 'warning',
+                        showConfirmButton: false
+                    }).then(function() {
+                        $("#dari").focus();
+                    });
+                    return false;
+                } else if (start.getTime() > end.getTime()) {
+                    swal({
+                        title: 'Oops',
+                        text: 'Periode tidak Valid !',
+                        icon: 'warning',
+                        showConfirmButton: false
+                    }).then(function() {
+                        $("#dari").focus();
+                    });
+                    return false;
+                } else if (start.getTime() < datestart.getTime()) {
+                    swal({
+                        title: 'Oops',
+                        text: 'Periode tidak Valid !',
+                        icon: 'warning',
+                        showConfirmButton: false
+                    }).then(function() {
+                        $("#dari").focus();
+                    });
+                    return false;
+                } else {
+                    return true;
+                }
+            });
         });
-    });
-
-</script>
+    </script>
 @endpush
